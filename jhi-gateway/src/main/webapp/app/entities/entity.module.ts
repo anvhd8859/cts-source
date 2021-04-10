b8fd3361e28f5826e97a176021af7defcc93b98f
hainm149889 <43931722+anvhd8859@users.noreply.github.com>
2021-04-11 01:43:06 +0700

    Update entity.module.ts

--- jhi-gateway/src/main/webapp/app/entities/entity.module.ts
+++ jhi-gateway/src/main/webapp/app/entities/entity.module.ts
@@ -1,10 +1,20 @@
 import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 
+import { CtsgatewayUserProfileModule } from './user-profile/user-profile.module';
+import { CtsgatewayDistrictModule as CtsmicroserviceDistrictModule } from './ctsmicroservice/district/district.module';
+import { CtsgatewayProvinceModule as CtsmicroserviceProvinceModule } from './ctsmicroservice/province/province.module';
+import { CtsgatewayStreetModule as CtsmicroserviceStreetModule } from './ctsmicroservice/street/street.module';
+import { CtsgatewaySubDistrictModule as CtsmicroserviceSubDistrictModule } from './ctsmicroservice/sub-district/sub-district.module';
 /* jhipster-needle-add-entity-module-import - JHipster will add entity modules imports here */
 
 @NgModule({
     // prettier-ignore
     imports: [
+        CtsgatewayUserProfileModule,
+        CtsmicroserviceDistrictModule,
+        CtsmicroserviceProvinceModule,
+        CtsmicroserviceStreetModule,
+        CtsmicroserviceSubDistrictModule,
         /* jhipster-needle-add-entity-module - JHipster will add entity modules here */
     ],
     declarations: [],
