4df37584797a84bf348a7623d7ab855fb5391e47
hainm149889 <43931722+anvhd8859@users.noreply.github.com>
2021-04-11 01:47:59 +0700

    Update webpack.dev.js

--- jhi-gateway/webpack/webpack.dev.js
+++ jhi-gateway/webpack/webpack.dev.js
@@ -19,6 +19,7 @@ module.exports = (options) => webpackMerge(commonConfig({ env: ENV }), {
         contentBase: './build/www',
         proxy: [{
             context: [
+                '/ctsmicroservice',
                 /* jhipster-needle-add-entity-to-webpack - JHipster will add entity api paths here */
                 '/api',
                 '/management',
