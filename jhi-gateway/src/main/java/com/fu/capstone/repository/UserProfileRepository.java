c2bd0b0ad69cbf07384712304e7eb034191a199d
hainm149889 <43931722+anvhd8859@users.noreply.github.com>
2021-04-11 22:41:28 +0700

    Update UserProfileRepository.java

--- jhi-gateway/src/main/java/com/fu/capstone/repository/UserProfileRepository.java
+++ jhi-gateway/src/main/java/com/fu/capstone/repository/UserProfileRepository.java
@@ -1,7 +1,11 @@
 package com.fu.capstone.repository;
 
 import com.fu.capstone.domain.UserProfile;
+
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.*;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 
@@ -11,5 +15,8 @@ import org.springframework.stereotype.Repository;
 @SuppressWarnings("unused")
 @Repository
 public interface UserProfileRepository extends JpaRepository<UserProfile, Long> {
+
+	@Query(value = "SELECT profile FROM UserProfile profile WHERE profile.userId = :id ")
+	Optional<UserProfile> findByUserId(@Param("id")Long id);
 
 }
