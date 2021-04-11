977aacf79e8c185e07e28f9a8c26bf665d5ef950
anvhd8859 <43931722+anvhd8859@users.noreply.github.com>
2021-04-11 23:13:56 +0700

    commit get invoice details by header id in service

--- jhi-srv/src/main/java/com/fu/capstone/service/InvoiceDetailsService.java
+++ jhi-srv/src/main/java/com/fu/capstone/service/InvoiceDetailsService.java
@@ -5,6 +5,7 @@ import com.fu.capstone.service.dto.InvoiceDetailsDTO;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
+import java.util.List;
 import java.util.Optional;
 
 /**
@@ -43,4 +44,7 @@ public interface InvoiceDetailsService {
      * @param id the id of the entity
      */
     void delete(Long id);
+
+    // AnhVD new code
+	List<InvoiceDetailsDTO> getInvoiceDetailsByHeaderId(Long id);
 }
