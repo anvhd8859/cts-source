4584f4b31a6e46216d69497a8406d7cc3a76ef4f
hainm149889 <43931722+anvhd8859@users.noreply.github.com>
2021-04-11 22:41:51 +0700

    Update account.service.ts

--- jhi-gateway/src/main/webapp/app/core/auth/account.service.ts
+++ jhi-gateway/src/main/webapp/app/core/auth/account.service.ts
@@ -3,16 +3,14 @@ import { HttpClient, HttpResponse } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import * as moment from 'moment';
 import { SERVER_API_URL } from 'app/app.constants';
-import { IDistrict } from 'app/shared/model/ctsmicroservice/district.model';
-import { ISubDistrict } from 'app/shared/model/ctsmicroservice/sub-district.model';
-import { IStreet } from 'app/shared/model/ctsmicroservice/street.model';
 import { IProvince } from 'app/shared/model/ctsmicroservice/province.model';
+import { IUserProfile } from 'app/shared/model/user-profile.model';
 import { createRequestOption } from 'app/shared';
-import { map } from 'rxjs/operators';
 
 @Injectable({ providedIn: 'root' })
 export class AccountService {
     public locationResourceUrl = SERVER_API_URL + 'ctsmicroservice/api';
+    public profileResourceUrl = SERVER_API_URL + 'api/user-profiles';
 
     constructor(private http: HttpClient) {}
 
@@ -39,6 +37,30 @@ export class AccountService {
 
     getLstStreetByWard(req?: any): Observable<HttpResponse<any[]>> {
         return this.http.get<any[]>(this.locationResourceUrl + '/streets/by-sub-district', { params: req, observe: 'response' });
+    }
+
+    createUserProfile(userProfile: IUserProfile): Observable<HttpResponse<IUserProfile>> {
+        return this.http.post<IUserProfile>(this.profileResourceUrl, userProfile, { observe: 'response' }).pipe();
+    }
+
+    updateUserProfile(userProfile: IUserProfile): Observable<HttpResponse<IUserProfile>> {
+        return this.http.put<IUserProfile>(this.profileResourceUrl, userProfile, { observe: 'response' }).pipe();
+    }
+
+    findByUserID(req: any): Observable<HttpResponse<IUserProfile>> {
+        return this.http.get<IUserProfile>(this.profileResourceUrl + '/find-by-user-id', { params: req, observe: 'response' }).pipe();
+    }
+
+    getAllDistrict(req?: any): Observable<HttpResponse<any[]>> {
+        return this.http.get<any[]>(this.locationResourceUrl + '/districts?size=9999', { observe: 'response' }).pipe();
+    }
+
+    getAllSubDistrict(req?: any): Observable<HttpResponse<any[]>> {
+        return this.http.get<any[]>(this.locationResourceUrl + '/sub-districts?size=9999', { observe: 'response' }).pipe();
+    }
+
+    getAllStreet(req?: any): Observable<HttpResponse<any[]>> {
+        return this.http.get<any[]>(this.locationResourceUrl + '/streets?size=9999', { observe: 'response' }).pipe();
     }
 
     private convertDateArrayFromServer(res: HttpResponse<any[]>): HttpResponse<any[]> {
