0493b6cc10c6ccfd37043fda750537eeea8bb88e
hainm149889 <43931722+anvhd8859@users.noreply.github.com>
2021-04-11 22:41:45 +0700

    Update settings.component.ts

--- jhi-gateway/src/main/webapp/app/account/settings/settings.component.ts
+++ jhi-gateway/src/main/webapp/app/account/settings/settings.component.ts
@@ -1,16 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, OnDestroy, OnInit } from '@angular/core';
 
 import { Principal, AccountService } from 'app/core';
+import { UserProfileService } from 'app/entities/user-profile';
 import { IDistrict } from 'app/shared/model/ctsmicroservice/district.model';
 import { IProvince } from 'app/shared/model/ctsmicroservice/province.model';
 import { IStreet } from 'app/shared/model/ctsmicroservice/street.model';
 import { ISubDistrict } from 'app/shared/model/ctsmicroservice/sub-district.model';
+import { IUserProfile, UserProfile } from 'app/shared/model/user-profile.model';
+import { forkJoin } from 'rxjs';
 
 @Component({
     selector: 'jhi-settings',
     templateUrl: './settings.component.html'
 })
-export class SettingsComponent implements OnInit {
+export class SettingsComponent implements OnInit, AfterViewInit {
     error: string;
     success: string;
     settingsAccount: any;
@@ -24,15 +27,45 @@ export class SettingsComponent implements OnInit {
     selectedSubDistrict: any;
     selectedStreet: any;
     selectedAddress: any;
+    lstGender: any = [{ id: 'Male', text: 'Male' }, { id: 'Female', text: 'Female' }, { id: 'Other', text: 'Other' }];
+    selectedGender: any;
+    userProfile: IUserProfile;
+    selectedPhoneNumber: string;
 
-    constructor(private accountService: AccountService, private principal: Principal) {}
+    constructor(private accountService: AccountService, private principal: Principal, private userProfileService: UserProfileService) {}
 
-    ngOnInit() {
+    ngAfterViewInit() {
         this.principal.identity().then(account => {
             this.settingsAccount = this.copyAccount(account);
-            this.getLstCity();
+            this.accountService.findByUserID({ id: account.id }).subscribe(res => {
+                this.userProfile = res.body;
+                if (!this.userProfile) {
+                    this.userProfile = new UserProfile();
+                    this.getLstCity();
+                } else {
+                    const streetId = this.userProfile.streetId;
+                    forkJoin(
+                        this.accountService.getLstCity(),
+                        this.accountService.getAllDistrict(),
+                        this.accountService.getAllSubDistrict(),
+                        this.accountService.getAllStreet()
+                    ).subscribe(res => {
+                        this.lstProvince = res[0].body;
+                        // this.lstStreet           = res[3].body.find(e => e.id === streetId);
+                        // this.lstSubDistrict      = res[2].body.find(e => e.id === this.selectedStreet);
+                        // this.lstDistrict         = res[1].body.find(e => e.id === this.selectedSubDistrict);
+
+                        // this.selectedStreet      = this.lstStreet[0].id;
+                        // this.selectedSubDistrict = this.lstSubDistrict[0].id;
+                        // this.selectedDistrict    = this.lstDistrict[0].id;
+                        // this.selectedProvince    = res[0].body.find(e => e.id === this.selectedDistrict).id;
+                    });
+                }
+            });
         });
     }
+
+    ngOnInit() {}
 
     save() {
         this.accountService.save(this.settingsAccount).subscribe(
