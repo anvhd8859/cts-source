99c37325fb38dd410c9a30158ffa95f6a9f19da6
hainm149889 <43931722+anvhd8859@users.noreply.github.com>
2021-04-13 22:47:15 +0700

    Create index.ts

---
+++ jhi-gateway/src/main/webapp/app/entities/ctsmicroservice/invoice-header/index.ts
@@ -0,0 +1,6 @@
+export * from './invoice-header.service';
+export * from './invoice-header-update.component';
+export * from './invoice-header-delete-dialog.component';
+export * from './invoice-header-detail.component';
+export * from './invoice-header.component';
+export * from './invoice-header.route';
