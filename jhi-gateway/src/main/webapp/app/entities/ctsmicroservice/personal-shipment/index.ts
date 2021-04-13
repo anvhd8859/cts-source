ef15b54861c5bd5a58d5797ad0816dfcafa9a10a
hainm149889 <43931722+anvhd8859@users.noreply.github.com>
2021-04-13 22:54:20 +0700

    Create index.ts

---
+++ jhi-gateway/src/main/webapp/app/entities/ctsmicroservice/personal-shipment/index.ts
@@ -0,0 +1,6 @@
+export * from './personal-shipment.service';
+export * from './personal-shipment-update.component';
+export * from './personal-shipment-delete-dialog.component';
+export * from './personal-shipment-detail.component';
+export * from './personal-shipment.component';
+export * from './personal-shipment.route';
