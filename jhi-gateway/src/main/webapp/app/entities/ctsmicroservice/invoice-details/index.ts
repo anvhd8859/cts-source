109cf7a8c325ca3764fc70794c163f21a11f35e6
ThangND <thangndse05808@fpt.edu.vn>
2021-04-16 23:40:49 +0700

    Delete index.ts

--- jhi-gateway/src/main/webapp/app/entities/ctsmicroservice/invoice-details/index.ts
+++
@@ -1,6 +0,0 @@
-export * from './invoice-details.service';
-export * from './invoice-details-update.component';
-export * from './invoice-details-delete-dialog.component';
-export * from './invoice-details-detail.component';
-export * from './invoice-details.component';
-export * from './invoice-details.route';
