a8b8c272f8ae81bb3996bc9da859aff3cddd46e5
ThangND <thangndse05808@fpt.edu.vn>
2021-04-16 23:44:56 +0700

    Create invoice-package.module.ts

---
+++ jhi-gateway/src/main/webapp/app/entities/ctsmicroservice/invoice-header/invoice-package/invoice-package.module.ts
@@ -0,0 +1,34 @@
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
+import { CtsgatewaySharedModule } from 'app/shared';
+import {
+    InvoicePackageComponent,
+    InvoicePackageDetailComponent,
+    InvoicePackageUpdateComponent,
+    InvoicePackageDeletePopupComponent,
+    InvoicePackageDeleteDialogComponent,
+    invoicePackageRoute,
+    invoicePackagePopupRoute
+} from '.';
+
+const ENTITY_STATES = [...invoicePackageRoute, ...invoicePackagePopupRoute];
+
+@NgModule({
+    imports: [CtsgatewaySharedModule, RouterModule.forChild(ENTITY_STATES)],
+    declarations: [
+        InvoicePackageComponent,
+        InvoicePackageDetailComponent,
+        InvoicePackageUpdateComponent,
+        InvoicePackageDeleteDialogComponent,
+        InvoicePackageDeletePopupComponent
+    ],
+    entryComponents: [
+        InvoicePackageComponent,
+        InvoicePackageUpdateComponent,
+        InvoicePackageDeleteDialogComponent,
+        InvoicePackageDeletePopupComponent
+    ],
+    schemas: [CUSTOM_ELEMENTS_SCHEMA]
+})
+export class CtsgatewayInvoicePackageModule {}
