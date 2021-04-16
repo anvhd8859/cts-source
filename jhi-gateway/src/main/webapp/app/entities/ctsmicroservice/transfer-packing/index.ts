306b38a718782d586e0ec085ea6acc5a97da1b54
ThangND <thangndse05808@fpt.edu.vn>
2021-04-16 23:50:37 +0700

    Delete index.ts

--- jhi-gateway/src/main/webapp/app/entities/ctsmicroservice/transfer-packing/index.ts
+++
@@ -1,6 +0,0 @@
-export * from './transfer-packing.service';
-export * from './transfer-packing-update.component';
-export * from './transfer-packing-delete-dialog.component';
-export * from './transfer-packing-detail.component';
-export * from './transfer-packing.component';
-export * from './transfer-packing.route';
