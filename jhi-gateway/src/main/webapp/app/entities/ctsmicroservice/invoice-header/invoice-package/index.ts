14128e659659b831a87fd61e20d48fd4ab1c5e74
ThangND <thangndse05808@fpt.edu.vn>
2021-04-16 23:43:53 +0700

    Create index.ts

---
+++ jhi-gateway/src/main/webapp/app/entities/ctsmicroservice/invoice-header/invoice-package/index.ts
@@ -0,0 +1,6 @@
+export * from './invoice-package.service';
+export * from './invoice-package-update.component';
+export * from './invoice-package-delete-dialog.component';
+export * from './invoice-package-detail.component';
+export * from './invoice-package.component';
+export * from './invoice-package.route';
