787bbb413bdcf74b48301e77ac9eaf56c2a75ab2
tuyenvnt <77369982+tuyenvnt@users.noreply.github.com>
2021-04-16 15:32:20 +0700

    Update PaymentRepository.java

--- jhi-srv/src/main/java/com/fu/capstone/repository/PaymentRepository.java
+++ jhi-srv/src/main/java/com/fu/capstone/repository/PaymentRepository.java
@@ -14,7 +14,7 @@ public interface PaymentRepository extends JpaRepository<Payment, Long> {
 
 	// START TuyenVNT 14/04/2021
 	@Query( value="SELECT * FROM payment WHERE invoice_header_id = :invoiceHeaderId"
-			countQuery = "SELECT count(p) FROM Payment p WHERE p.invoiceHeaderId = :invoiceHeaderId", 
+			countQuery = "SELECT count(*) FROM payment WHERE invoice_header_id = :invoiceHeaderId", 
 			nativeQuery = true)
 	Page<Payment> getPaymentByHeaderId(@Param("invoiceHeaderId") Long id,Pageable pageable);
 	// END TuyenVNT 16/04/2021
