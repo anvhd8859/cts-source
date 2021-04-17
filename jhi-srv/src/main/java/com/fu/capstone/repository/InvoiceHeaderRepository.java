81f4a65912d8e6292165803951b2dfac2f6dc20c
dongph1-13012021 <77370027+dongph1-13012021@users.noreply.github.com>
2021-04-17 14:27:50 +0700

    Update InvoiceHeaderRepository.java

--- jhi-srv/src/main/java/com/fu/capstone/repository/InvoiceHeaderRepository.java
+++ jhi-srv/src/main/java/com/fu/capstone/repository/InvoiceHeaderRepository.java
@@ -35,13 +35,13 @@ public interface InvoiceHeaderRepository extends JpaRepository<InvoiceHeader, Lo
 	@Query( value = "SELECT i.* FROM invoice_header i, personal_shipment ps, employee e, person p "
 				  + " WHERE i.id = ps.invoice_header_id AND ps.employee_id = e.id AND e.person_id = p.id AND p.email = :userName "
 				  + " AND ps.status <> 'finish' "
-				  + " AND (ps.shipment_type = '' OR ps.shipment_type = :type) "
-				  + " AND (i.invoice_no = '' OR i.invoice_no like '%:invNo%') ",
-				  countQuery =  "SELECT COUNT(i.*) FROM invoice_header i, personal_shipment ps, employee e, person p "
+				  + " AND (:type = '' OR ps.shipment_type = :type) "
+				  + " AND (:invNo = '' OR i.invoice_no like CONCAT('%', :invNo , '%')) ",
+				  countQuery =  "SELECT COUNT(*) FROM invoice_header i, personal_shipment ps, employee e, person p "
 						  + " WHERE i.id = ps.invoice_header_id AND ps.employee_id = e.id AND e.person_id = p.id AND p.email = :userName "
 						  + " AND ps.status <> 'finish' "
-						  + " AND (ps.shipment_type = '' OR ps.shipment_type = :type) "
-						  + " AND (i.invoice_no = '' OR i.invoice_no like '%:invNo%') ",
+						  + " AND (:type = '' OR ps.shipment_type = :type) "
+						  + " AND (:invNo = '' OR i.invoice_no like CONCAT('%', :invNo , '%')) ",
 				  nativeQuery = true)
 	Page<InvoiceHeader> getInvoiceHeadersByShipper (@Param("userName") String userName, @Param("invNo") String invNo, @Param("type") String type, Pageable pageable );
 
