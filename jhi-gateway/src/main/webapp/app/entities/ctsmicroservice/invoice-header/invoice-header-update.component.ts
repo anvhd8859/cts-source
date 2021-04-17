e9cfc73946a55026a638c1589e3ac68fdf3dd9bc
ThangND <thangndse05808@fpt.edu.vn>
2021-04-17 14:26:17 +0700

    Update invoice-header-update.component.ts

--- jhi-gateway/src/main/webapp/app/entities/ctsmicroservice/invoice-header/invoice-header-update.component.ts
+++ jhi-gateway/src/main/webapp/app/entities/ctsmicroservice/invoice-header/invoice-header-update.component.ts
@@ -1,12 +1,17 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { HttpResponse, HttpErrorResponse } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { forkJoin, Observable } from 'rxjs';
 import * as moment from 'moment';
 import { DATE_TIME_FORMAT } from 'app/shared/constants/input.constants';
 
 import { IInvoiceHeader } from 'app/shared/model/ctsmicroservice/invoice-header.model';
 import { InvoiceHeaderService } from './invoice-header.service';
+import { AccountService, IUser } from 'app/core';
+import { IDistrict } from 'app/shared/model/ctsmicroservice/district.model';
+import { IProvince } from 'app/shared/model/ctsmicroservice/province.model';
+import { IStreet } from 'app/shared/model/ctsmicroservice/street.model';
+import { ISubDistrict } from 'app/shared/model/ctsmicroservice/sub-district.model';
 
 @Component({
     selector: 'jhi-invoice-header-update',
@@ -19,8 +24,33 @@ export class InvoiceHeaderUpdateComponent implements OnInit {
     finishDate: string;
     createDate: string;
     updateDate: string;
+    lstUser: IUser[] = [];
+    lstProvinceFrom: IProvince[] = [];
+    lstDistrictFrom: IDistrict[] = [];
+    lstSubDistrictFrom: ISubDistrict[] = [];
+    lstStreetFrom: IStreet[] = [];
+    lstProvinceTo: IProvince[] = [];
+    lstDistrictTo: IDistrict[] = [];
+    lstSubDistrictTo: ISubDistrict[] = [];
+    lstStreetTo: IStreet[] = [];
+    selectedProvinceFrom: any;
+    selectedDistrictFrom: any;
+    selectedSubDistrictFrom: any;
+    selectedStreetFrom: any;
+    selectedAddressFrom: any;
+    selectedProvinceTo: any;
+    selectedDistrictTo: any;
+    selectedSubDistrictTo: any;
+    selectedStreetTo: any;
+    selectedAddressTo: any;
+    lstIvnType: any = [{ id: 'Personal', text: 'Personal Shippemnt' }, { id: 'Transfer', text: 'House Transfer' }];
+    lstStatus: any = [{ id: 'New', text: 'New' }, { id: 'Shipped', text: 'Shipped' }, { id: 'Cancelled', text: 'Cancelled' }];
 
-    constructor(private invoiceHeaderService: InvoiceHeaderService, private activatedRoute: ActivatedRoute) {}
+    constructor(
+        private invoiceHeaderService: InvoiceHeaderService,
+        private accountService: AccountService,
+        private activatedRoute: ActivatedRoute
+    ) {}
 
     ngOnInit() {
         this.isSaving = false;
@@ -30,6 +60,11 @@ export class InvoiceHeaderUpdateComponent implements OnInit {
             this.finishDate = this.invoiceHeader.finishDate != null ? this.invoiceHeader.finishDate.format(DATE_TIME_FORMAT) : null;
             this.createDate = this.invoiceHeader.createDate != null ? this.invoiceHeader.createDate.format(DATE_TIME_FORMAT) : null;
             this.updateDate = this.invoiceHeader.updateDate != null ? this.invoiceHeader.updateDate.format(DATE_TIME_FORMAT) : null;
+        });
+        forkJoin(this.invoiceHeaderService.getLstUser(), this.accountService.getLstCity()).subscribe(res => {
+            this.lstUser = res[0].body.filter(e => e.authorities.filter(i => i === 'ROLE_USER'));
+            this.lstProvinceFrom = res[1].body;
+            this.lstProvinceTo = res[1].body;
         });
     }
 
@@ -49,6 +84,74 @@ export class InvoiceHeaderUpdateComponent implements OnInit {
             this.subscribeToSaveResponse(this.invoiceHeaderService.create(this.invoiceHeader));
         }
     }
+
+    // ThangND Start
+    changeCity(opt: any) {
+        if (opt === 'from') {
+            this.lstDistrictFrom = null;
+            this.selectedDistrictFrom = null;
+            this.lstSubDistrictFrom = null;
+            this.selectedSubDistrictFrom = null;
+            this.lstStreetFrom = null;
+            this.selectedStreetFrom = null;
+            const param = { provinceId: this.selectedProvinceFrom.id };
+            this.accountService.getLstDistrictByCity(param).subscribe(res => {
+                this.lstDistrictFrom = res.body;
+            });
+        } else if (opt === 'to') {
+            this.lstDistrictTo = null;
+            this.selectedDistrictTo = null;
+            this.lstSubDistrictTo = null;
+            this.selectedSubDistrictTo = null;
+            this.lstStreetTo = null;
+            this.selectedStreetTo = null;
+            const param = { provinceId: this.selectedProvinceTo.id };
+            this.accountService.getLstDistrictByCity(param).subscribe(res => {
+                this.lstDistrictTo = res.body;
+            });
+        }
+    }
+
+    changeDistrict(opt: any) {
+        if (opt === 'from') {
+            this.lstSubDistrictFrom = null;
+            this.selectedSubDistrictFrom = null;
+            this.lstStreetFrom = null;
+            this.selectedStreetFrom = null;
+            const param = { id: this.selectedDistrictFrom.id };
+            this.accountService.getLstWardByDistrict(param).subscribe(res => {
+                this.lstSubDistrictFrom = res.body;
+            });
+        } else if (opt === 'to') {
+            this.lstSubDistrictTo = null;
+            this.selectedSubDistrictTo = null;
+            this.lstStreetTo = null;
+            this.selectedStreetTo = null;
+            const param = { id: this.selectedDistrictTo.id };
+            this.accountService.getLstWardByDistrict(param).subscribe(res => {
+                this.lstSubDistrictTo = res.body;
+            });
+        }
+    }
+
+    changeSubDistrict(opt: any) {
+        if (opt === 'from') {
+            this.lstStreetFrom = null;
+            this.selectedStreetFrom = null;
+            const param = { id: this.selectedSubDistrictFrom.id };
+            this.accountService.getLstStreetByWard(param).subscribe(res => {
+                this.lstStreetFrom = res.body;
+            });
+        } else if (opt === 'to') {
+            this.lstStreetTo = null;
+            this.selectedStreetTo = null;
+            const param = { id: this.selectedSubDistrictTo.id };
+            this.accountService.getLstStreetByWard(param).subscribe(res => {
+                this.lstStreetTo = res.body;
+            });
+        }
+    }
+    // ThangND End
 
     private subscribeToSaveResponse(result: Observable<HttpResponse<IInvoiceHeader>>) {
         result.subscribe((res: HttpResponse<IInvoiceHeader>) => this.onSaveSuccess(), (res: HttpErrorResponse) => this.onSaveError());
