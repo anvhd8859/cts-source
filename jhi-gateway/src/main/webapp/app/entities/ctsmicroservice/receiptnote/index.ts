6ac0276d2acbb31c629c5676988e0d1a395d239d
anvhd8859 <43931722+anvhd8859@users.noreply.github.com>
2021-04-17 21:15:39 +0700

    Delete index.ts

--- jhi-gateway/src/main/webapp/app/entities/ctsmicroservice/receiptnote/index.ts
+++
@@ -1,6 +0,0 @@
-export * from './receiptnote.service';
-export * from './receiptnote-update.component';
-export * from './receiptnote-delete-dialog.component';
-export * from './receiptnote-detail.component';
-export * from './receiptnote.component';
-export * from './receiptnote.route';
