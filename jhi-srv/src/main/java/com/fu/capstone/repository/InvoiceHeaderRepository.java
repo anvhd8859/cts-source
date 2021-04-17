b7642a98bf5fc1ba04e4b903cdff81d8cfc03af2
dongph1-13012021 <77370027+dongph1-13012021@users.noreply.github.com>
2021-04-17 11:41:10 +0700

    Update InvoiceHeaderRepository.java

--- jhi-srv/src/main/java/com/fu/capstone/repository/InvoiceHeaderRepository.java
+++ jhi-srv/src/main/java/com/fu/capstone/repository/InvoiceHeaderRepository.java
@@ -34,7 +34,7 @@ public interface InvoiceHeaderRepository extends JpaRepository<InvoiceHeader, Lo
 	
 	@Query( value = "SELECT i.* FROM invoice_header i, personal_shipment ps, employee e, person p "
 				  + " WHERE i.id = ps.invoice_header_id AND ps.employee_id = e.id AND e.person_id = p.id AND p.email = :userName "
-				  + " AND ps.status <> 'Hoàn thành' AND ps.shipment_type = :type ",
+				  + " AND ps.status <> 'finish' AND (ps.shipment_type = '' OR ps.shipment_type = :type) ",
 				  nativeQuery = true)
 	Page<InvoiceHeader> getInvoiceHeadersByShipperId (@Param("userName") String userName, @Param("type")  String type, Pageable pageable );
 
