f837797260cca8a57fffa03c14f9ccbb097f4fba
ThangND <thangndse05808@fpt.edu.vn>
2021-04-18 15:09:10 +0700

    Update receiptnote-detail.component.ts

--- jhi-gateway/src/main/webapp/app/entities/ctsmicroservice/invoice-header/receiptnote/receiptnote-detail.component.ts
+++ jhi-gateway/src/main/webapp/app/entities/ctsmicroservice/invoice-header/receiptnote/receiptnote-detail.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
+import { IUser, Principal } from 'app/core';
 
 import { IReceiptnote } from 'app/shared/model/ctsmicroservice/receiptnote.model';
 
@@ -9,16 +10,25 @@ import { IReceiptnote } from 'app/shared/model/ctsmicroservice/receiptnote.model
 })
 export class ReceiptnoteDetailComponent implements OnInit {
     receiptnote: IReceiptnote;
+    currentUser: IUser;
 
-    constructor(private activatedRoute: ActivatedRoute) {}
+    constructor(private activatedRoute: ActivatedRoute, private principal: Principal, private router: Router) {}
 
     ngOnInit() {
+        this.principal.identity().then(account => {
+            this.currentUser = account;
+        });
         this.activatedRoute.data.subscribe(({ receiptnote }) => {
             this.receiptnote = receiptnote;
         });
     }
 
     previousState() {
-        window.history.back();
+        const condition = this.currentUser.authorities.find(e => e === 'ROLE_ADMIN');
+        if (condition) {
+            this.router.navigate(['/invoice-header']);
+        } else {
+            window.history.back();
+        }
     }
 }
