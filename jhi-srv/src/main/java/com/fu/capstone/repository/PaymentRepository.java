9b1172c9a697b72fb49c4888db2d6a95fd25e84c
tuyenvnt <77369982+tuyenvnt@users.noreply.github.com>
2021-04-20 15:18:44 +0700

    Update PaymentRepository.java

--- jhi-srv/src/main/java/com/fu/capstone/repository/PaymentRepository.java
+++ jhi-srv/src/main/java/com/fu/capstone/repository/PaymentRepository.java
@@ -1,7 +1,14 @@
 package com.fu.capstone.repository;
 
 import com.fu.capstone.domain.Payment;
+import com.fu.capstone.service.dto.PaymentDTO;
+
+import java.util.Optional;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.*;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 
@@ -13,7 +20,7 @@ import org.springframework.stereotype.Repository;
 public interface PaymentRepository extends JpaRepository<Payment, Long> {
 
 	// START TuyenVNT 14/04/2021
-	@Query( value="SELECT * FROM payment WHERE invoice_header_id = :invoiceHeaderId"
+	@Query( value="SELECT * FROM payment WHERE invoice_header_id = :invoiceHeaderId",
 			countQuery = "SELECT count(*) FROM payment WHERE invoice_header_id = :invoiceHeaderId", 
 			nativeQuery = true)
 	Page<Payment> getPaymentByHeaderId(@Param("invoiceHeaderId") Long id,Pageable pageable);
