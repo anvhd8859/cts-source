440e16e03142df0ee13b19a6fa06cae3eafb3658
dongph1-13012021 <77370027+dongph1-13012021@users.noreply.github.com>
2021-04-20 21:28:06 +0700

    Create index.ts

---
+++ jhi-gateway/src/main/webapp/app/entities/ctsmicroservice/cancel-invoice/index.ts
@@ -0,0 +1,6 @@
+export * from './cancel-invoice.service';
+export * from './cancel-invoice-approve-dialog.component';
+export * from './cancel-invoice-delete-dialog.component';
+export * from './cancel-invoice-detail.component';
+export * from './cancel-invoice.component';
+export * from './cancel-invoice.route';
