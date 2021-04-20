2ad033b819f95630fb332e7bd89c889d685d4518
tuyenvnt <77369982+tuyenvnt@users.noreply.github.com>
2021-04-20 15:19:03 +0700

    Update PersonalShipmentRepository.java

--- jhi-srv/src/main/java/com/fu/capstone/repository/PersonalShipmentRepository.java
+++ jhi-srv/src/main/java/com/fu/capstone/repository/PersonalShipmentRepository.java
@@ -1,7 +1,14 @@
 package com.fu.capstone.repository;
 
 import com.fu.capstone.domain.PersonalShipment;
+import com.fu.capstone.service.dto.PersonalShipmentDTO;
+
+import java.util.Optional;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.*;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 
