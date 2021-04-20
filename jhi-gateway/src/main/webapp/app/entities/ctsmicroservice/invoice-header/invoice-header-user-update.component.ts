8d780ff73dbbdf419e1275b85bb040bbe19fa6fc
HaiNM <hainm149889@gmail.com>
2021-04-21 00:25:33 +0700

    Update invoice-header-user-update.component.ts

--- jhi-gateway/src/main/webapp/app/entities/ctsmicroservice/invoice-header/invoice-header-user-update.component.ts
+++ jhi-gateway/src/main/webapp/app/entities/ctsmicroservice/invoice-header/invoice-header-user-update.component.ts
@@ -14,6 +14,7 @@ import { IStreet } from 'app/shared/model/ctsmicroservice/street.model';
 import { ISubDistrict } from 'app/shared/model/ctsmicroservice/sub-district.model';
 import { JhiAlertService } from 'ng-jhipster';
 import { IUserProfile } from 'app/shared/model/user-profile.model';
+import { IInvoiceDetails, InvoiceDetails } from 'app/shared/model/ctsmicroservice/invoice-details.model';
 
 @Component({
     selector: 'jhi-invoice-header-user-update',
