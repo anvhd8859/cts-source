e2301cb634426fc88a0aab44a6b05694aed0b9aa
dongph1-13012021 <77370027+dongph1-13012021@users.noreply.github.com>
2021-04-21 23:56:46 +0700

    Revert "Create index.ts"
    This reverts commit b50e3da0b116adab19829906ed139711f548e2f1.

--- jhi-gateway/src/main/webapp/app/entities/ctsmicroservice/confirm-receipt-note/index.ts
+++
@@ -1,6 +0,0 @@
-export * from './confirm-receipt-note.service';
-export * from './confirm-receipt-note-update.component';
-export * from './confirm-receipt-note-delete-dialog.component';
-export * from './confirm-receipt-note-detail.component';
-export * from './confirm-receipt-note.component';
-export * from './confirm-receipt-note.route';
