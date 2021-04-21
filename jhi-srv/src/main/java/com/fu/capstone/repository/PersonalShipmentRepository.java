94766ff978ea331dbce781b5a83987f5d3468e71
dongph1-13012021 <77370027+dongph1-13012021@users.noreply.github.com>
2021-04-21 18:52:59 +0700

    Update PersonalShipmentRepository.java

--- jhi-srv/src/main/java/com/fu/capstone/repository/PersonalShipmentRepository.java
+++ jhi-srv/src/main/java/com/fu/capstone/repository/PersonalShipmentRepository.java
@@ -1,15 +1,21 @@
 package com.fu.capstone.repository;
 
 import com.fu.capstone.domain.PersonalShipment;
+
+import java.util.List;
+
 import org.springframework.data.jpa.repository.*;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 
 /**
  * Spring Data  repository for the PersonalShipment entity.
  */
-@SuppressWarnings("unused")
 @Repository
 public interface PersonalShipmentRepository extends JpaRepository<PersonalShipment, Long> {
+
+	@Query( value = "SELECT p FROM PersonalShipment p WHERE p.invoiceHeaderId = :invoiceId")
+	List<PersonalShipment> getShipmentByInvoice(@Param("invoiceId") Long id);
 
 }
