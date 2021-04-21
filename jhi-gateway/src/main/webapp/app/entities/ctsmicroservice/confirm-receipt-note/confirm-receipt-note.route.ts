08310df16f1debfb870b802657b44bc545a35db6
dongph1-13012021 <77370027+dongph1-13012021@users.noreply.github.com>
2021-04-22 00:00:02 +0700

    Revert "Revert "Create confirm-receipt-note.route.ts""
    This reverts commit 86940563f413080d688cedd7200de5e48fc45c0f.

---
+++ jhi-gateway/src/main/webapp/app/entities/ctsmicroservice/confirm-receipt-note/confirm-receipt-note.route.ts
@@ -0,0 +1,95 @@
+import { Injectable } from '@angular/core';
+import { HttpResponse } from '@angular/common/http';
+import { Resolve, ActivatedRouteSnapshot, RouterStateSnapshot, Routes } from '@angular/router';
+import { JhiPaginationUtil, JhiResolvePagingParams } from 'ng-jhipster';
+import { UserRouteAccessService } from 'app/core';
+import { of } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { ConfirmReceiptNote } from 'app/shared/model/ctsmicroservice/confirm-receipt-note.model';
+import { ConfirmReceiptNoteService } from './confirm-receipt-note.service';
+import { ConfirmReceiptNoteComponent } from './confirm-receipt-note.component';
+import { ConfirmReceiptNoteDetailComponent } from './confirm-receipt-note-detail.component';
+import { ConfirmReceiptNoteUpdateComponent } from './confirm-receipt-note-update.component';
+import { ConfirmReceiptNoteDeletePopupComponent } from './confirm-receipt-note-delete-dialog.component';
+import { IConfirmReceiptNote } from 'app/shared/model/ctsmicroservice/confirm-receipt-note.model';
+
+@Injectable({ providedIn: 'root' })
+export class ConfirmReceiptNoteResolve implements Resolve<IConfirmReceiptNote> {
+    constructor(private service: ConfirmReceiptNoteService) {}
+
+    resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
+        const id = route.params['id'] ? route.params['id'] : null;
+        if (id) {
+            return this.service.find(id).pipe(map((confirmReceiptNote: HttpResponse<ConfirmReceiptNote>) => confirmReceiptNote.body));
+        }
+        return of(new ConfirmReceiptNote());
+    }
+}
+
+export const confirmReceiptNoteRoute: Routes = [
+    {
+        path: 'confirm-receipt-note',
+        component: ConfirmReceiptNoteComponent,
+        resolve: {
+            pagingParams: JhiResolvePagingParams
+        },
+        data: {
+            authorities: ['ROLE_USER'],
+            defaultSort: 'id,asc',
+            pageTitle: 'ConfirmReceiptNotes'
+        },
+        canActivate: [UserRouteAccessService]
+    },
+    {
+        path: 'confirm-receipt-note/:id/view',
+        component: ConfirmReceiptNoteDetailComponent,
+        resolve: {
+            confirmReceiptNote: ConfirmReceiptNoteResolve
+        },
+        data: {
+            authorities: ['ROLE_USER'],
+            pageTitle: 'ConfirmReceiptNotes'
+        },
+        canActivate: [UserRouteAccessService]
+    },
+    {
+        path: 'confirm-receipt-note/new',
+        component: ConfirmReceiptNoteUpdateComponent,
+        resolve: {
+            confirmReceiptNote: ConfirmReceiptNoteResolve
+        },
+        data: {
+            authorities: ['ROLE_USER'],
+            pageTitle: 'ConfirmReceiptNotes'
+        },
+        canActivate: [UserRouteAccessService]
+    },
+    {
+        path: 'confirm-receipt-note/:id/edit',
+        component: ConfirmReceiptNoteUpdateComponent,
+        resolve: {
+            confirmReceiptNote: ConfirmReceiptNoteResolve
+        },
+        data: {
+            authorities: ['ROLE_USER'],
+            pageTitle: 'ConfirmReceiptNotes'
+        },
+        canActivate: [UserRouteAccessService]
+    }
+];
+
+export const confirmReceiptNotePopupRoute: Routes = [
+    {
+        path: 'confirm-receipt-note/:id/delete',
+        component: ConfirmReceiptNoteDeletePopupComponent,
+        resolve: {
+            confirmReceiptNote: ConfirmReceiptNoteResolve
+        },
+        data: {
+            authorities: ['ROLE_USER'],
+            pageTitle: 'ConfirmReceiptNotes'
+        },
+        canActivate: [UserRouteAccessService],
+        outlet: 'popup'
+    }
+];
