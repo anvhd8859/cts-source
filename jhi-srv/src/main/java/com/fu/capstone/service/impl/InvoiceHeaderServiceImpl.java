856e03c81f32556f6f55c775b3bddf1d943b5254
dongph1-13012021 <77370027+dongph1-13012021@users.noreply.github.com>
2021-04-21 15:11:28 +0700

    Update InvoiceHeaderServiceImpl.java

--- jhi-srv/src/main/java/com/fu/capstone/service/impl/InvoiceHeaderServiceImpl.java
+++ jhi-srv/src/main/java/com/fu/capstone/service/impl/InvoiceHeaderServiceImpl.java
@@ -5,10 +5,12 @@ import com.fu.capstone.domain.InvoiceDetails;
 import com.fu.capstone.domain.InvoiceHeader;
 import com.fu.capstone.domain.InvoicePackage;
 import com.fu.capstone.domain.PersonalShipment;
+import com.fu.capstone.domain.Street;
 import com.fu.capstone.repository.InvoiceDetailsRepository;
 import com.fu.capstone.repository.InvoiceHeaderRepository;
 import com.fu.capstone.repository.InvoicePackageRepository;
 import com.fu.capstone.repository.PersonalShipmentRepository;
+import com.fu.capstone.repository.StreetRepository;
 import com.fu.capstone.service.dto.InvoiceHeaderDTO;
 import com.fu.capstone.service.dto.InvoicePackageDetailDTO;
 import com.fu.capstone.service.mapper.InvoiceDetailsMapper;
@@ -24,6 +26,7 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
@@ -45,6 +48,8 @@ public class InvoiceHeaderServiceImpl implements InvoiceHeaderService {
 	
 	private PersonalShipmentRepository personalShipmentRepository;
 	
+	private StreetRepository streetRepository;
+	
 	private InvoiceHeaderMapper invoiceHeaderMapper;
 	
 	private InvoiceDetailsMapper invoiceDetailsMapper;
@@ -54,7 +59,7 @@ public class InvoiceHeaderServiceImpl implements InvoiceHeaderService {
 	public InvoiceHeaderServiceImpl(InvoiceHeaderRepository invoiceHeaderRepository, InvoiceHeaderMapper invoiceHeaderMapper, 
 			InvoiceDetailsRepository invoiceDetailsRepository, InvoiceDetailsMapper invoiceDetailsMapper, 
 			InvoicePackageRepository invoicePackageRepository, InvoicePackageMapper invoicePackageMapper,
-			PersonalShipmentRepository personalShipmentRepository) {
+			PersonalShipmentRepository personalShipmentRepository, StreetRepository streetRepository) {
 		this.invoiceHeaderRepository = invoiceHeaderRepository;
 		this.invoiceHeaderMapper = invoiceHeaderMapper;
 		this.invoiceDetailsRepository = invoiceDetailsRepository;
@@ -62,6 +67,7 @@ public class InvoiceHeaderServiceImpl implements InvoiceHeaderService {
 		this.invoicePackageRepository = invoicePackageRepository;
 		this.invoicePackageMapper = invoicePackageMapper;
 		this.personalShipmentRepository = personalShipmentRepository;
+		this.streetRepository = streetRepository;
 	}
 
 	/**
@@ -156,10 +162,22 @@ public class InvoiceHeaderServiceImpl implements InvoiceHeaderService {
 
 	@Override
 	public InvoiceHeaderDTO createInvoiceHeaderDetailPackage(InvoicePackageDetailDTO invoicePackageDetailDTO) {
+		// get data from web
 		InvoiceHeader invoiceHeader = invoiceHeaderMapper.toEntity(invoicePackageDetailDTO.getHeader());
+		List<InvoiceDetails> lstDetail = invoiceDetailsMapper.toEntity(invoicePackageDetailDTO.getLstDetail());
+		List<InvoicePackage> lstPackage = invoicePackageMapper.toEntity(invoicePackageDetailDTO.getLstPackage());
+		Street fromStreet = streetRepository.getFullAddressByStreetId(invoiceHeader.getStartStreetId());
+		Street toStreet = streetRepository.getFullAddressByStreetId(invoiceHeader.getDestinationStreetId());
+		
+		// process data
+		BigDecimal subTotal = calculateSubTotal(lstPackage, fromStreet, toStreet);
+		invoiceHeader.setSubTotal(subTotal);
+		invoiceHeader.setTaxAmount(subTotal.multiply(new BigDecimal(0.1)));
+		invoiceHeader.setTotalDue(subTotal.add(invoiceHeader.getTaxAmount()));
+		
+		// save data
 		invoiceHeader = invoiceHeaderRepository.save(invoiceHeader);
 		InvoiceHeaderDTO invoiceHeaderDTO = invoiceHeaderMapper.toDto(invoiceHeader);
-		
 		if(invoiceHeaderDTO.getStatus().equalsIgnoreCase("collect")){
 			PersonalShipment psOne = new PersonalShipment();
 			psOne.setStatus("waiting");
@@ -173,22 +191,43 @@ public class InvoiceHeaderServiceImpl implements InvoiceHeaderService {
 			lstShipment.add(psOne);
 			lstShipment.add(psTwo);
 			personalShipmentRepository.saveAll(lstShipment);
-		}
-		else {
+		} else {
 			PersonalShipment psTwo = new PersonalShipment();
 			psTwo.setStatus("waiting");
 			psTwo.setInvoiceHeaderId(invoiceHeaderDTO.getId());
 			psTwo.setShipmentType("delivery");
 			personalShipmentRepository.save(psTwo);
 		}
-		
-		List<InvoiceDetails> lstDetail = invoiceDetailsMapper.toEntity(invoicePackageDetailDTO.getLstDetail());
 		invoiceDetailsRepository.saveAll(lstDetail);
-		
-		List<InvoicePackage> lstPackage = invoicePackageMapper.toEntity(invoicePackageDetailDTO.getLstPackage());
-		invoicePackageRepository.saveAll(lstPackage);
-		
+		invoicePackageRepository.saveAll(lstPackage);		
 		return invoiceHeaderDTO;
+	}
+	
+	private BigDecimal calculateSubTotal(List<InvoicePackage> lstPackage, Street fromStreet, Street toStreet) {
+		BigDecimal result = new BigDecimal(0);
+		float totalWeight = 0;
+		for (InvoicePackage ip : lstPackage) {
+			totalWeight += ip.getWeight();
+		}
+		if(fromStreet.getSubDistrictId().getDistrictId().getProvinceId().getId() == 
+				toStreet.getSubDistrictId().getDistrictId().getProvinceId().getId()) {
+			if(totalWeight <= 0.25) result = new BigDecimal(9000);
+			else if(totalWeight <= 0.50) result = new BigDecimal(13000);
+			else if(totalWeight <= 1.00) result = new BigDecimal(16000);
+			else if(totalWeight <= 1.50) result = new BigDecimal(25000);
+			else if(totalWeight <= 2.00) result = new BigDecimal(29000);
+			else if(totalWeight <= 100.00) result = new BigDecimal(2600.0 * totalWeight);
+			else result = new BigDecimal(1400.0 * totalWeight);
+		} else {
+			if(totalWeight <= 0.25) result = new BigDecimal(10000);
+			else if(totalWeight <= 0.50) result = new BigDecimal(14000);
+			else if(totalWeight <= 1.00) result = new BigDecimal(17000);
+			else if(totalWeight <= 1.50) result = new BigDecimal(26000);
+			else if(totalWeight <= 2.00) result = new BigDecimal(30000);
+			else if(totalWeight <= 100.00) result = new BigDecimal(5000.0 * totalWeight);
+			else result = new BigDecimal(3200.0 * totalWeight);
+		}
+		return result;
 	}
 
 }
