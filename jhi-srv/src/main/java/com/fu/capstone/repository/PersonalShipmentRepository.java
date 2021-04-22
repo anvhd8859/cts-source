649e0daeaa1f4b810d7e1095dc4e69d0e974ac91
dongph1-13012021 <77370027+dongph1-13012021@users.noreply.github.com>
2021-04-22 13:05:13 +0700

    Revert "Update PersonalShipmentRepository.java"
    This reverts commit 38002fdf1e4092dd57e18ef0ef2611f722817036.

--- jhi-srv/src/main/java/com/fu/capstone/repository/PersonalShipmentRepository.java
+++ jhi-srv/src/main/java/com/fu/capstone/repository/PersonalShipmentRepository.java
@@ -2,6 +2,7 @@ package com.fu.capstone.repository;
 
 import com.fu.capstone.domain.PersonalShipment;
 
+import java.util.Optional;
 import java.util.List;
 
 import org.springframework.data.domain.Page;
@@ -30,7 +31,7 @@ public interface PersonalShipmentRepository extends JpaRepository<PersonalShipme
 	// END TuyenVNT 
 
 
-    // new code DongPH
+  // new code DongPH
 	@Query( value = "SELECT p FROM PersonalShipment p WHERE p.invoiceHeaderId = :invoiceId")
 	List<PersonalShipment> getShipmentByInvoice(@Param("invoiceId") Long id);
 
