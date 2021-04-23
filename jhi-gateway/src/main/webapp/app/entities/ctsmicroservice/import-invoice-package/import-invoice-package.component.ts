91addfe36c81b0bae6ddb54cf9349334783551a9
dongph1-13012021 <77370027+dongph1-13012021@users.noreply.github.com>
2021-04-23 14:49:34 +0700

    Create import-invoice-package.component.ts

---
+++ jhi-gateway/src/main/webapp/app/entities/ctsmicroservice/import-invoice-package/import-invoice-package.component.ts
@@ -0,0 +1,58 @@
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { HttpErrorResponse, HttpResponse } from '@angular/common/http';
+import { Subscription } from 'rxjs';
+import { JhiEventManager, JhiAlertService } from 'ng-jhipster';
+
+import { IImportInvoicePackage } from 'app/shared/model/ctsmicroservice/import-invoice-package.model';
+import { Principal } from 'app/core';
+import { ImportInvoicePackageService } from './import-invoice-package.service';
+
+@Component({
+    selector: 'jhi-import-invoice-package',
+    templateUrl: './import-invoice-package.component.html'
+})
+export class ImportInvoicePackageComponent implements OnInit, OnDestroy {
+    importInvoicePackages: IImportInvoicePackage[];
+    currentAccount: any;
+    eventSubscriber: Subscription;
+
+    constructor(
+        private importInvoicePackageService: ImportInvoicePackageService,
+        private jhiAlertService: JhiAlertService,
+        private eventManager: JhiEventManager,
+        private principal: Principal
+    ) {}
+
+    loadAll() {
+        this.importInvoicePackageService.query().subscribe(
+            (res: HttpResponse<IImportInvoicePackage[]>) => {
+                this.importInvoicePackages = res.body;
+            },
+            (res: HttpErrorResponse) => this.onError(res.message)
+        );
+    }
+
+    ngOnInit() {
+        this.loadAll();
+        this.principal.identity().then(account => {
+            this.currentAccount = account;
+        });
+        this.registerChangeInImportInvoicePackages();
+    }
+
+    ngOnDestroy() {
+        this.eventManager.destroy(this.eventSubscriber);
+    }
+
+    trackId(index: number, item: IImportInvoicePackage) {
+        return item.id;
+    }
+
+    registerChangeInImportInvoicePackages() {
+        this.eventSubscriber = this.eventManager.subscribe('importInvoicePackageListModification', response => this.loadAll());
+    }
+
+    private onError(errorMessage: string) {
+        this.jhiAlertService.error(errorMessage, null, null);
+    }
+}
