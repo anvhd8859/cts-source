b7c18e5b14283b410beb16c3807c4be3ca075331
dongph1-13012021 <77370027+dongph1-13012021@users.noreply.github.com>
2021-04-23 14:49:52 +0700

    Create index.ts

---
+++ jhi-gateway/src/main/webapp/app/entities/ctsmicroservice/import-invoice-package/index.ts
@@ -0,0 +1,6 @@
+export * from './import-invoice-package.service';
+export * from './import-invoice-package-update.component';
+export * from './import-invoice-package-delete-dialog.component';
+export * from './import-invoice-package-detail.component';
+export * from './import-invoice-package.component';
+export * from './import-invoice-package.route';
