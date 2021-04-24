1801873ed528cffa69824be0341493e081415e67
dongph1-13012021 <77370027+dongph1-13012021@users.noreply.github.com>
2021-04-24 21:21:00 +0700

    Update import-invoice-package.service.ts

--- jhi-gateway/src/main/webapp/app/entities/ctsmicroservice/import-invoice-package/import-invoice-package.service.ts
+++ jhi-gateway/src/main/webapp/app/entities/ctsmicroservice/import-invoice-package/import-invoice-package.service.ts
@@ -7,10 +7,11 @@ import { map } from 'rxjs/operators';
 
 import { SERVER_API_URL } from 'app/app.constants';
 import { createRequestOption } from 'app/shared';
-import { IImportInvoicePackage } from 'app/shared/model/ctsmicroservice/import-invoice-package.model';
+import { IInvoicePackageShipment } from '.';
+import { IInvoiceHeader } from 'app/shared/model/ctsmicroservice/invoice-header.model';
 
-type EntityResponseType = HttpResponse<IImportInvoicePackage>;
-type EntityArrayResponseType = HttpResponse<IImportInvoicePackage[]>;
+type EntityResponseType = HttpResponse<IInvoicePackageShipment>;
+type EntityArrayResponseType = HttpResponse<IInvoicePackageShipment[]>;
 
 @Injectable({ providedIn: 'root' })
 export class ImportInvoicePackageService {
@@ -18,30 +19,30 @@ export class ImportInvoicePackageService {
 
     constructor(private http: HttpClient) {}
 
-    create(importInvoicePackage: IImportInvoicePackage): Observable<EntityResponseType> {
-        const copy = this.convertDateFromClient(importInvoicePackage);
+    create(importInvoicePackage: IInvoicePackageShipment): Observable<EntityResponseType> {
+        const copy = importInvoicePackage;
         return this.http
-            .post<IImportInvoicePackage>(this.resourceUrl, copy, { observe: 'response' })
+            .post<IInvoicePackageShipment>(this.resourceUrl, copy, { observe: 'response' })
             .pipe(map((res: EntityResponseType) => this.convertDateFromServer(res)));
     }
 
-    update(importInvoicePackage: IImportInvoicePackage): Observable<EntityResponseType> {
-        const copy = this.convertDateFromClient(importInvoicePackage);
+    update(importInvoicePackage: IInvoicePackageShipment): Observable<EntityResponseType> {
+        const copy = importInvoicePackage;
         return this.http
-            .put<IImportInvoicePackage>(this.resourceUrl, copy, { observe: 'response' })
+            .put<IInvoicePackageShipment>(this.resourceUrl, copy, { observe: 'response' })
             .pipe(map((res: EntityResponseType) => this.convertDateFromServer(res)));
     }
 
-    find(id: number): Observable<EntityResponseType> {
+    find(id: number): Observable<any> {
         return this.http
-            .get<IImportInvoicePackage>(`${this.resourceUrl}/${id}`, { observe: 'response' })
-            .pipe(map((res: EntityResponseType) => this.convertDateFromServer(res)));
+            .get<IInvoiceHeader>(`${SERVER_API_URL + 'ctsmicroservice/api/invoice-headers'}/${id}`, { observe: 'response' })
+            .pipe(map((res: any) => this.invoiceDateFromServer(res)));
     }
 
     query(req?: any): Observable<EntityArrayResponseType> {
         const options = createRequestOption(req);
         return this.http
-            .get<IImportInvoicePackage[]>(this.resourceUrl, { params: options, observe: 'response' })
+            .get<IInvoicePackageShipment[]>(this.resourceUrl, { params: options, observe: 'response' })
             .pipe(map((res: EntityArrayResponseType) => this.convertDateArrayFromServer(res)));
     }
 
@@ -49,30 +50,82 @@ export class ImportInvoicePackageService {
         return this.http.delete<any>(`${this.resourceUrl}/${id}`, { observe: 'response' });
     }
 
-    private convertDateFromClient(importInvoicePackage: IImportInvoicePackage): IImportInvoicePackage {
-        const copy: IImportInvoicePackage = Object.assign({}, importInvoicePackage, {
-            createDate:
-                importInvoicePackage.createDate != null && importInvoicePackage.createDate.isValid()
-                    ? importInvoicePackage.createDate.toJSON()
-                    : null,
-            updateDate:
-                importInvoicePackage.updateDate != null && importInvoicePackage.updateDate.isValid()
-                    ? importInvoicePackage.updateDate.toJSON()
-                    : null
-        });
-        return copy;
+    // get Import Package By Office Id and Status
+    getImportPackage(req?: any): Observable<EntityArrayResponseType> {
+        const options = createRequestOption(req);
+        return this.http
+            .get<IInvoicePackageShipment[]>(this.resourceUrl + '/import-package', { params: options, observe: 'response' })
+            .pipe(map((res: EntityArrayResponseType) => this.convertDateArrayFromServer(res)));
+    }
+
+    // update list invoice, package, shipment
+    updateListInvoicePackageShipment(req?: any): Observable<EntityResponseType> {
+        const copy = req;
+        return this.http
+            .put<IInvoicePackageShipment>(this.resourceUrl, copy, { observe: 'response' })
+            .pipe(map((res: EntityResponseType) => this.convertDateFromServer(res)));
+    }
+
+    private invoiceDateFromServer(res: any): any {
+        res.body.createDate = res.body.createDate != null ? moment(res.body.createDate) : null;
+        res.body.updateDate = res.body.updateDate != null ? moment(res.body.updateDate) : null;
     }
 
     private convertDateFromServer(res: EntityResponseType): EntityResponseType {
-        res.body.createDate = res.body.createDate != null ? moment(res.body.createDate) : null;
-        res.body.updateDate = res.body.updateDate != null ? moment(res.body.updateDate) : null;
+        res.body.invoiceHeader.dueDate = res.body.invoiceHeader.dueDate != null ? moment(res.body.invoiceHeader.dueDate) : null;
+        res.body.invoiceHeader.finishDate = res.body.invoiceHeader.finishDate != null ? moment(res.body.invoiceHeader.finishDate) : null;
+        res.body.invoiceHeader.createDate = res.body.invoiceHeader.createDate != null ? moment(res.body.invoiceHeader.createDate) : null;
+        res.body.invoiceHeader.updateDate = res.body.invoiceHeader.updateDate != null ? moment(res.body.invoiceHeader.updateDate) : null;
+
+        res.body.personalShipment.shipTime = res.body.personalShipment.shipTime != null ? moment(res.body.personalShipment.shipTime) : null;
+        res.body.personalShipment.finishTime =
+            res.body.personalShipment.finishTime != null ? moment(res.body.personalShipment.finishTime) : null;
+        res.body.personalShipment.createDate =
+            res.body.personalShipment.createDate != null ? moment(res.body.personalShipment.createDate) : null;
+        res.body.personalShipment.updateDate =
+            res.body.personalShipment.updateDate != null ? moment(res.body.personalShipment.updateDate) : null;
+
+        for (let i in res.body.invoicePackageList) {
+            let crDate = res.body.invoicePackageList[i].createDate;
+            let udDate = res.body.invoicePackageList[i].updateDate;
+            res.body.invoicePackageList[i].createDate = crDate != null ? moment(crDate) : null;
+            res.body.invoicePackageList[i].updateDate = udDate != null ? moment(udDate) : null;
+        }
         return res;
     }
 
     private convertDateArrayFromServer(res: EntityArrayResponseType): EntityArrayResponseType {
-        res.body.forEach((importInvoicePackage: IImportInvoicePackage) => {
-            importInvoicePackage.createDate = importInvoicePackage.createDate != null ? moment(importInvoicePackage.createDate) : null;
-            importInvoicePackage.updateDate = importInvoicePackage.updateDate != null ? moment(importInvoicePackage.updateDate) : null;
+        res.body.forEach((invoicePackageShipment: IInvoicePackageShipment) => {
+            invoicePackageShipment.invoiceHeader.dueDate =
+                invoicePackageShipment.invoiceHeader.dueDate != null ? moment(invoicePackageShipment.invoiceHeader.dueDate) : null;
+            invoicePackageShipment.invoiceHeader.finishDate =
+                invoicePackageShipment.invoiceHeader.finishDate != null ? moment(invoicePackageShipment.invoiceHeader.finishDate) : null;
+            invoicePackageShipment.invoiceHeader.createDate =
+                invoicePackageShipment.invoiceHeader.createDate != null ? moment(invoicePackageShipment.invoiceHeader.createDate) : null;
+            invoicePackageShipment.invoiceHeader.updateDate =
+                invoicePackageShipment.invoiceHeader.updateDate != null ? moment(invoicePackageShipment.invoiceHeader.updateDate) : null;
+
+            invoicePackageShipment.personalShipment.shipTime =
+                invoicePackageShipment.personalShipment.shipTime != null ? moment(invoicePackageShipment.personalShipment.shipTime) : null;
+            invoicePackageShipment.personalShipment.finishTime =
+                invoicePackageShipment.personalShipment.finishTime != null
+                    ? moment(invoicePackageShipment.personalShipment.finishTime)
+                    : null;
+            invoicePackageShipment.personalShipment.createDate =
+                invoicePackageShipment.personalShipment.createDate != null
+                    ? moment(invoicePackageShipment.personalShipment.createDate)
+                    : null;
+            invoicePackageShipment.personalShipment.updateDate =
+                invoicePackageShipment.personalShipment.updateDate != null
+                    ? moment(invoicePackageShipment.personalShipment.updateDate)
+                    : null;
+
+            for (let i in invoicePackageShipment.invoicePackageList) {
+                let crDate = invoicePackageShipment.invoicePackageList[i].createDate;
+                let udDate = invoicePackageShipment.invoicePackageList[i].updateDate;
+                invoicePackageShipment.invoicePackageList[i].createDate = crDate != null ? moment(crDate) : null;
+                invoicePackageShipment.invoicePackageList[i].updateDate = udDate != null ? moment(udDate) : null;
+            }
         });
         return res;
     }
