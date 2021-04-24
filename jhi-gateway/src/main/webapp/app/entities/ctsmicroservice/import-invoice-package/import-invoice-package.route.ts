d9110542973211d2158d5373c049051ff0bca6e0
dongph1-13012021 <77370027+dongph1-13012021@users.noreply.github.com>
2021-04-24 21:20:54 +0700

    Update import-invoice-package.route.ts

--- jhi-gateway/src/main/webapp/app/entities/ctsmicroservice/import-invoice-package/import-invoice-package.route.ts
+++ jhi-gateway/src/main/webapp/app/entities/ctsmicroservice/import-invoice-package/import-invoice-package.route.ts
@@ -3,25 +3,25 @@ import { HttpResponse } from '@angular/common/http';
 import { Resolve, ActivatedRouteSnapshot, RouterStateSnapshot, Routes } from '@angular/router';
 import { UserRouteAccessService } from 'app/core';
 import { of } from 'rxjs';
-import { map } from 'rxjs/operators';
 import { ImportInvoicePackage } from 'app/shared/model/ctsmicroservice/import-invoice-package.model';
 import { ImportInvoicePackageService } from './import-invoice-package.service';
 import { ImportInvoicePackageComponent } from './import-invoice-package.component';
-import { ImportInvoicePackageDetailComponent } from './import-invoice-package-detail.component';
-import { ImportInvoicePackageUpdateComponent } from './import-invoice-package-update.component';
-import { ImportInvoicePackageDeletePopupComponent } from './import-invoice-package-delete-dialog.component';
 import { IImportInvoicePackage } from 'app/shared/model/ctsmicroservice/import-invoice-package.model';
+import { ImportInvoicePackageImportPopupComponent } from './import-invoice-package-import-dialog.component';
+import { InvoiceHeader } from 'app/shared/model/ctsmicroservice/invoice-header.model';
+import { map } from 'rxjs/operators';
+import { JhiResolvePagingParams } from 'ng-jhipster';
 
 @Injectable({ providedIn: 'root' })
-export class ImportInvoicePackageResolve implements Resolve<IImportInvoicePackage> {
+export class ImportInvoicePackageResolve implements Resolve<InvoiceHeader> {
     constructor(private service: ImportInvoicePackageService) {}
 
     resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
         const id = route.params['id'] ? route.params['id'] : null;
         if (id) {
-            return this.service.find(id).pipe(map((importInvoicePackage: HttpResponse<ImportInvoicePackage>) => importInvoicePackage.body));
+            return this.service.find(id).pipe(map((invoice: HttpResponse<InvoiceHeader>) => invoice.body));
         }
-        return of(new ImportInvoicePackage());
+        return of(new InvoiceHeader());
     }
 }
 
@@ -29,41 +29,8 @@ export const importInvoicePackageRoute: Routes = [
     {
         path: 'import-invoice-package',
         component: ImportInvoicePackageComponent,
-        data: {
-            authorities: ['ROLE_USER'],
-            pageTitle: 'ImportInvoicePackages'
-        },
-        canActivate: [UserRouteAccessService]
-    },
-    {
-        path: 'import-invoice-package/:id/view',
-        component: ImportInvoicePackageDetailComponent,
         resolve: {
-            importInvoicePackage: ImportInvoicePackageResolve
-        },
-        data: {
-            authorities: ['ROLE_USER'],
-            pageTitle: 'ImportInvoicePackages'
-        },
-        canActivate: [UserRouteAccessService]
-    },
-    {
-        path: 'import-invoice-package/new',
-        component: ImportInvoicePackageUpdateComponent,
-        resolve: {
-            importInvoicePackage: ImportInvoicePackageResolve
-        },
-        data: {
-            authorities: ['ROLE_USER'],
-            pageTitle: 'ImportInvoicePackages'
-        },
-        canActivate: [UserRouteAccessService]
-    },
-    {
-        path: 'import-invoice-package/:id/edit',
-        component: ImportInvoicePackageUpdateComponent,
-        resolve: {
-            importInvoicePackage: ImportInvoicePackageResolve
+            pagingParams: JhiResolvePagingParams
         },
         data: {
             authorities: ['ROLE_USER'],
@@ -75,8 +42,8 @@ export const importInvoicePackageRoute: Routes = [
 
 export const importInvoicePackagePopupRoute: Routes = [
     {
-        path: 'import-invoice-package/:id/delete',
-        component: ImportInvoicePackageDeletePopupComponent,
+        path: 'import-invoice-package/:id/import',
+        component: ImportInvoicePackageImportPopupComponent,
         resolve: {
             importInvoicePackage: ImportInvoicePackageResolve
         },
