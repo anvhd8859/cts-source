5c956a0405ef8477c0fb57c791008245f84dcfe6
Hainm149889 <hainm149889@gmail.com>
2021-04-26 02:36:17 +0700

    Update index.ts

--- jhi-gateway/src/main/webapp/app/entities/ctsmicroservice/invoice-header/personal-shipment/index.ts
+++ jhi-gateway/src/main/webapp/app/entities/ctsmicroservice/invoice-header/personal-shipment/index.ts
@@ -3,4 +3,5 @@ export * from './personal-shipment-update.component';
 export * from './personal-shipment-delete-dialog.component';
 export * from './personal-shipment-detail.component';
 export * from './personal-shipment.component';
+export * from './personal-shipment-admin.component';
 export * from './personal-shipment.route';
