7402fc3f684df5a870f424ee495380556caedb1c
Hainm149889 <hainm149889@gmail.com>
2021-04-25 16:23:45 +0700

    Update invoice-header-detail.component.ts

--- jhi-gateway/src/main/webapp/app/entities/ctsmicroservice/invoice-header/invoice-header-detail.component.ts
+++ jhi-gateway/src/main/webapp/app/entities/ctsmicroservice/invoice-header/invoice-header-detail.component.ts
@@ -1,8 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { AccountService } from 'app/core';
+import { AccountService, User } from 'app/core';
+import { IInvoiceDetails } from 'app/shared/model/ctsmicroservice/invoice-details.model';
 
 import { IInvoiceHeader } from 'app/shared/model/ctsmicroservice/invoice-header.model';
+import { IInvoicePackage } from 'app/shared/model/ctsmicroservice/invoice-package.model';
+import { forkJoin } from 'rxjs';
+import { InvoiceHeaderService } from '.';
 
 @Component({
     selector: 'jhi-invoice-header-detail',
@@ -10,12 +14,28 @@ import { IInvoiceHeader } from 'app/shared/model/ctsmicroservice/invoice-header.
 })
 export class InvoiceHeaderDetailComponent implements OnInit {
     invoiceHeader: IInvoiceHeader;
+    lstInvoicePackage: IInvoicePackage[] = [];
+    lstInvoiceDetails: IInvoiceDetails[] = [];
+    customer: User;
 
-    constructor(private activatedRoute: ActivatedRoute, private accountService: AccountService) {}
+    constructor(
+        private invoiceHeaderService: InvoiceHeaderService,
+        private activatedRoute: ActivatedRoute,
+        private accountService: AccountService
+    ) {}
 
     ngOnInit() {
         this.activatedRoute.data.subscribe(({ invoiceHeader }) => {
             this.invoiceHeader = invoiceHeader;
+            forkJoin(
+                this.invoiceHeaderService.getPackageByInvoiceId({ id: this.invoiceHeader.id }),
+                this.invoiceHeaderService.getDetailByInvoiceId({ id: this.invoiceHeader.id }),
+                this.invoiceHeaderService.getUserByID({ id: this.invoiceHeader.customerId })
+            ).subscribe(res => {
+                this.lstInvoicePackage = res[0].body;
+                this.lstInvoiceDetails = res[1].body;
+                this.customer = res[2].body;
+            });
         });
     }
 
