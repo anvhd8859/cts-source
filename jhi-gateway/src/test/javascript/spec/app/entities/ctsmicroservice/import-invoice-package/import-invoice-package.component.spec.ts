50e3499526987240b6bbe2d90910c16b0184f692
anvhd8859 <43931722+anvhd8859@users.noreply.github.com>
2021-04-27 00:31:31 +0700

    Create import-invoice-package.component.spec.ts

---
+++ jhi-gateway/src/test/javascript/spec/app/entities/ctsmicroservice/import-invoice-package/import-invoice-package.component.spec.ts
@@ -0,0 +1,51 @@
+/* tslint:disable max-line-length */
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { Observable, of } from 'rxjs';
+import { HttpHeaders, HttpResponse } from '@angular/common/http';
+
+import { CtsgatewayTestModule } from '../../../../test.module';
+import { ImportInvoicePackageComponent } from 'app/entities/ctsmicroservice/import-invoice-package/import-invoice-package.component';
+import { ImportInvoicePackageService } from 'app/entities/ctsmicroservice/import-invoice-package/import-invoice-package.service';
+import { ImportInvoicePackage } from 'app/shared/model/ctsmicroservice/import-invoice-package.model';
+
+describe('Component Tests', () => {
+    describe('ImportInvoicePackage Management Component', () => {
+        let comp: ImportInvoicePackageComponent;
+        let fixture: ComponentFixture<ImportInvoicePackageComponent>;
+        let service: ImportInvoicePackageService;
+
+        beforeEach(() => {
+            TestBed.configureTestingModule({
+                imports: [CtsgatewayTestModule],
+                declarations: [ImportInvoicePackageComponent],
+                providers: []
+            })
+                .overrideTemplate(ImportInvoicePackageComponent, '')
+                .compileComponents();
+
+            fixture = TestBed.createComponent(ImportInvoicePackageComponent);
+            comp = fixture.componentInstance;
+            service = fixture.debugElement.injector.get(ImportInvoicePackageService);
+        });
+
+        it('Should call load all on init', () => {
+            // GIVEN
+            const headers = new HttpHeaders().append('link', 'link;link');
+            spyOn(service, 'query').and.returnValue(
+                of(
+                    new HttpResponse({
+                        body: [new ImportInvoicePackage(123)],
+                        headers
+                    })
+                )
+            );
+
+            // WHEN
+            comp.ngOnInit();
+
+            // THEN
+            expect(service.query).toHaveBeenCalled();
+            expect(true).toEqual(true);
+        });
+    });
+});
