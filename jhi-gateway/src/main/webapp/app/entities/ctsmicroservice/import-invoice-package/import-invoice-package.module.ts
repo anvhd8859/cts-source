654930811c74817036b2f7ae30c28d1949b7266b
anvhd8859 <43931722+anvhd8859@users.noreply.github.com>
2021-04-26 23:50:06 +0700

    Create import-invoice-package.module.ts

---
+++ jhi-gateway/src/main/webapp/app/entities/ctsmicroservice/import-invoice-package/import-invoice-package.module.ts
@@ -0,0 +1,37 @@
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
+import { NgSelectModule } from '@ng-select/ng-select';
+
+import { CtsgatewaySharedModule } from 'app/shared';
+import {
+    ImportInvoicePackageComponent,
+    importInvoicePackageRoute,
+    importInvoicePackagePopupRoute,
+    ImportInvoicePackageImportPopupComponent,
+    ImportInvoicePackageImportDialogComponent
+} from './';
+import { ImportInvoicePackageDetailComponent } from './import-invoice-package-detail.component';
+import { ImportInvoicePackageUpdateComponent } from './import-invoice-package-update.component';
+
+const ENTITY_STATES = [...importInvoicePackageRoute, ...importInvoicePackagePopupRoute];
+
+@NgModule({
+    imports: [CtsgatewaySharedModule, RouterModule.forChild(ENTITY_STATES), NgSelectModule, FormsModule],
+    declarations: [
+        ImportInvoicePackageComponent,
+        ImportInvoicePackageImportDialogComponent,
+        ImportInvoicePackageImportPopupComponent,
+        ImportInvoicePackageUpdateComponent,
+        ImportInvoicePackageDetailComponent
+    ],
+    entryComponents: [
+        ImportInvoicePackageComponent,
+        ImportInvoicePackageImportDialogComponent,
+        ImportInvoicePackageImportPopupComponent,
+        ImportInvoicePackageUpdateComponent,
+        ImportInvoicePackageDetailComponent
+    ],
+    schemas: [CUSTOM_ELEMENTS_SCHEMA]
+})
+export class CtsgatewayImportInvoicePackageModule {}
