0364416e85973141177c17a217e242a2e1a58f05
anvhd8859 <43931722+anvhd8859@users.noreply.github.com>
2021-04-27 00:34:23 +0700

    Update StreetRepository.java

--- jhi-srv/src/main/java/com/fu/capstone/repository/StreetRepository.java
+++ jhi-srv/src/main/java/com/fu/capstone/repository/StreetRepository.java
@@ -24,6 +24,9 @@ public interface StreetRepository extends JpaRepository<Street, Long> {
 	Page<Street> getAllStreetsBySubDistrictId(@Param("subDistrictId") Long id,Pageable pageable);
 
 	@Query( value = "SELECT s FROM Street s WHERE s.id = :id")
-	Optional<Street> getFullAddressByStreetId(@Param("id") Long id);
+	Street getFullAddressByStreetId(@Param("id") Long id);
+
+	@Query( value = "SELECT s FROM Street s, Office o WHERE s.id = o.streetId AND o.id = :id")
+	Street getAddressByOfficeId(@Param("id") Long id);
 
 }
