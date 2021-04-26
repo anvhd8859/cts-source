78c2240476440522b60c7acbff2187d1335ed393
anvhd8859 <43931722+anvhd8859@users.noreply.github.com>
2021-04-27 00:34:20 +0700

    Update ReceiptNoteRepository.java

--- jhi-srv/src/main/java/com/fu/capstone/repository/ReceiptNoteRepository.java
+++ jhi-srv/src/main/java/com/fu/capstone/repository/ReceiptNoteRepository.java
@@ -2,8 +2,10 @@ package com.fu.capstone.repository;
 
 import com.fu.capstone.domain.ReceiptNote;
 
+import java.util.List;
 import java.util.Optional;
 
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.*;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
@@ -15,7 +17,13 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface ReceiptNoteRepository extends JpaRepository<ReceiptNote, Long> {
 
-	@Query( value = " SELECT r FROM ReceiptNote r WHERE r.invoiceHeaderId = :id ")
+	@Query(value = " SELECT r FROM ReceiptNote r WHERE r.invoiceHeaderId = :id ")
 	Optional<ReceiptNote> getReceiptNoteByHeaderId(@Param("id") Long id);
+
+	@Query(value = " SELECT r FROM ReceiptNote r, InvoiceHeader i WHERE "
+				 + " r.invoiceHeaderId = i.id AND i.customerId = :id "
+				 + " AND (r.customerConfirm = FALSE OR r.customerConfirm is NULL) "
+				 + " ORDER BY r.createDate DESC")
+	List<ReceiptNote> getAllReceiptNotConfirm(@Param("id") Long id, Pageable pageable);
 
 }
