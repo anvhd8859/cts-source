b88a1743d635d0213460e8faf70e2bf7ef185405
anvhd8859 <43931722+anvhd8859@users.noreply.github.com>
2021-04-27 00:34:48 +0700

    Create PersonalShipmentInvoiceDTO.java

---
+++ jhi-srv/src/main/java/com/fu/capstone/service/dto/PersonalShipmentInvoiceDTO.java
@@ -0,0 +1,28 @@
+package com.fu.capstone.service.dto;
+
+import java.io.Serializable;
+
+public class PersonalShipmentInvoiceDTO implements Serializable  {
+	
+	private PersonalShipmentDTO personalShipmentDTO;
+	
+	private InvoiceHeaderDTO invoiceHeaderDTO;
+
+	public PersonalShipmentDTO getPersonalShipmentDTO() {
+		return personalShipmentDTO;
+	}
+
+	public void setPersonalShipmentDTO(PersonalShipmentDTO personalShipmentDTO) {
+		this.personalShipmentDTO = personalShipmentDTO;
+	}
+
+	public InvoiceHeaderDTO getInvoiceHeaderDTO() {
+		return invoiceHeaderDTO;
+	}
+
+	public void setInvoiceHeaderDTO(InvoiceHeaderDTO invoiceHeaderDTO) {
+		this.invoiceHeaderDTO = invoiceHeaderDTO;
+	}
+	
+	
+}
