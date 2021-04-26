e7cda7f1da6ce6c8dd715cbf2fc9984aa289c22a
anvhd8859 <43931722+anvhd8859@users.noreply.github.com>
2021-04-27 00:24:12 +0700

    Create office.component.ts

---
+++ jhi-gateway/src/main/webapp/app/entities/ctsmicroservice/office/office.component.ts
@@ -0,0 +1,58 @@
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { HttpErrorResponse, HttpResponse } from '@angular/common/http';
+import { Subscription } from 'rxjs';
+import { JhiEventManager, JhiAlertService } from 'ng-jhipster';
+
+import { IOffice } from 'app/shared/model/ctsmicroservice/office.model';
+import { Principal } from 'app/core';
+import { OfficeService } from './office.service';
+
+@Component({
+    selector: 'jhi-office',
+    templateUrl: './office.component.html'
+})
+export class OfficeComponent implements OnInit, OnDestroy {
+    offices: IOffice[];
+    currentAccount: any;
+    eventSubscriber: Subscription;
+
+    constructor(
+        private officeService: OfficeService,
+        private jhiAlertService: JhiAlertService,
+        private eventManager: JhiEventManager,
+        private principal: Principal
+    ) {}
+
+    loadAll() {
+        this.officeService.query().subscribe(
+            (res: HttpResponse<IOffice[]>) => {
+                this.offices = res.body;
+            },
+            (res: HttpErrorResponse) => this.onError(res.message)
+        );
+    }
+
+    ngOnInit() {
+        this.loadAll();
+        this.principal.identity().then(account => {
+            this.currentAccount = account;
+        });
+        this.registerChangeInOffices();
+    }
+
+    ngOnDestroy() {
+        this.eventManager.destroy(this.eventSubscriber);
+    }
+
+    trackId(index: number, item: IOffice) {
+        return item.id;
+    }
+
+    registerChangeInOffices() {
+        this.eventSubscriber = this.eventManager.subscribe('officeListModification', response => this.loadAll());
+    }
+
+    private onError(errorMessage: string) {
+        this.jhiAlertService.error(errorMessage, null, null);
+    }
+}
