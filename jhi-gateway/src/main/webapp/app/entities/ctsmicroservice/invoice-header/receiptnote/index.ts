f321a88d97376eb0b82a458581663cd9decdecdb
dongph1-13012021 <77370027+dongph1-13012021@users.noreply.github.com>
2021-04-26 23:01:44 +0700

    Update index.ts

--- jhi-gateway/src/main/webapp/app/entities/ctsmicroservice/invoice-header/receiptnote/index.ts
+++ jhi-gateway/src/main/webapp/app/entities/ctsmicroservice/invoice-header/receiptnote/index.ts
@@ -4,3 +4,5 @@ export * from './receiptnote-delete-dialog.component';
 export * from './receiptnote-detail.component';
 export * from './receiptnote.component';
 export * from './receiptnote.route';
+export * from './receipt-note-user.component';
+export * from './receipt-note-user-confirm-dialog.component';
