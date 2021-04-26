f7519435f5550040392c40afaa7cb3d12dca5776
anvhd8859 <43931722+anvhd8859@users.noreply.github.com>
2021-04-26 23:50:10 +0700

    Create import-invoice-package.route.ts

---
+++ jhi-gateway/src/main/webapp/app/entities/ctsmicroservice/import-invoice-package/import-invoice-package.route.ts
@@ -0,0 +1,57 @@
+import { IInvoiceHeader } from './../../../shared/model/ctsmicroservice/invoice-header.model';
+import { Injectable } from '@angular/core';
+import { HttpResponse } from '@angular/common/http';
+import { Resolve, ActivatedRouteSnapshot, RouterStateSnapshot, Routes } from '@angular/router';
+import { UserRouteAccessService } from 'app/core';
+import { of } from 'rxjs';
+import { ImportInvoicePackageService } from './import-invoice-package.service';
+import { ImportInvoicePackageComponent } from './import-invoice-package.component';
+import { ImportInvoicePackageImportPopupComponent } from './import-invoice-package-import-dialog.component';
+import { InvoiceHeader } from 'app/shared/model/ctsmicroservice/invoice-header.model';
+import { map } from 'rxjs/operators';
+import { JhiResolvePagingParams } from 'ng-jhipster';
+
+@Injectable({ providedIn: 'root' })
+export class ImportInvoicePackageResolve implements Resolve<any> {
+    invoice: IInvoiceHeader;
+    constructor(private service: ImportInvoicePackageService) {}
+
+    resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
+        const id = route.params['id'] ? route.params['id'] : null;
+        if (id) {
+            return of((new InvoiceHeader().id = id));
+        }
+        return of(new InvoiceHeader());
+    }
+}
+
+export const importInvoicePackageRoute: Routes = [
+    {
+        path: 'import-invoice-package',
+        component: ImportInvoicePackageComponent,
+        resolve: {
+            pagingParams: JhiResolvePagingParams
+        },
+        data: {
+            authorities: ['ROLE_KEEPER'],
+            pageTitle: 'ImportInvoicePackages'
+        },
+        canActivate: [UserRouteAccessService]
+    }
+];
+
+export const importInvoicePackagePopupRoute: Routes = [
+    {
+        path: 'import-invoice-package/:id/import',
+        component: ImportInvoicePackageImportPopupComponent,
+        resolve: {
+            importInvoicePackage: ImportInvoicePackageResolve
+        },
+        data: {
+            authorities: ['ROLE_KEEPER'],
+            pageTitle: 'ImportInvoicePackages'
+        },
+        canActivate: [UserRouteAccessService],
+        outlet: 'popup'
+    }
+];
