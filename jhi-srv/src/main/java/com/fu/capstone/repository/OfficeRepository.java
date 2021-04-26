b2b97e185b4b0e9d26b49cb3be1f0984717c938d
anvhd8859 <43931722+anvhd8859@users.noreply.github.com>
2021-04-27 00:34:09 +0700

    Update OfficeRepository.java

--- jhi-srv/src/main/java/com/fu/capstone/repository/OfficeRepository.java
+++ jhi-srv/src/main/java/com/fu/capstone/repository/OfficeRepository.java
@@ -15,8 +15,16 @@ public interface OfficeRepository extends JpaRepository<Office, Long> {
 	@Query( value =   " SELECT DISTINCT o.* FROM office o, street s, district d, sub_district sd "
 					+ " WHERE o.street_id = s.id AND d.id = sd.district_id_id AND sd.id = s.sub_district_id_id AND d.id = "
 					+ " ( SELECT d.id FROM district d, sub_district sd ,street s "
-					+ " WHERE d.id = sd.district_id_id AND sd.id = s.sub_district_id_id AND s.id = :id )",
+					+ " WHERE d.id = sd.district_id_id AND sd.id = s.sub_district_id_id AND s.id = :id LIMIT 1)",
 					nativeQuery = true)
 	Office getOfficeByStreetId(@Param("id")Long id);
+	
+	@Query( value = " SELECT o.* FROM office o, street s, district d, sub_district sd, province p "
+				  + " WHERE o.street_id = s.id AND s.sub_district_id_id = sd.id "
+				  + " AND sd.district_id_id = d.id AND d.province_id_id = p.id "
+				  + " AND ( s.id = :streetId OR sd.id = :subDistrictId OR d.id = :districtId OR p.id = :provinceId ) LIMIT 1",
+			nativeQuery = true)
+	Office searchOfficeNearby(@Param("streetId") Long streetId, @Param("subDistrictId") Long subDistrictId,
+			@Param("districtId") Long districtId, @Param("provinceId") Long provinceId);
 
 }
