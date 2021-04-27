581c3265cecf2ebf47aef4c139a2242f4b957920
anvhd8859 <43931722+anvhd8859@users.noreply.github.com>
2021-04-28 02:50:43 +0700

    Update receiptnote-update.component.ts

--- jhi-gateway/src/main/webapp/app/entities/ctsmicroservice/invoice-header/receiptnote/receiptnote-update.component.ts
+++ jhi-gateway/src/main/webapp/app/entities/ctsmicroservice/invoice-header/receiptnote/receiptnote-update.component.ts
@@ -9,6 +9,8 @@ import { IReceiptnote, Receiptnote } from 'app/shared/model/ctsmicroservice/rece
 import { ReceiptnoteService } from './receiptnote.service';
 import { IInvoiceHeader } from 'app/shared/model/ctsmicroservice/invoice-header.model';
 import { IUser, Principal } from 'app/core';
+import { IInvoicePackage, InvoicePackage } from 'app/shared/model/ctsmicroservice/invoice-package.model';
+import { IInvoiceDetails, InvoiceDetails } from 'app/shared/model/ctsmicroservice/invoice-details.model';
 
 @Component({
     selector: 'jhi-receiptnote-update',
@@ -22,28 +24,33 @@ export class ReceiptnoteUpdateComponent implements OnInit {
     updateDate: string;
     currentUser: IUser;
 
-    constructor(private receiptnoteService: ReceiptnoteService, private activatedRoute: ActivatedRoute, private principal: Principal) {}
+    lstInvoicePackage: IInvoicePackage[] = [];
+    invPackageCount: number;
+    lstInvoiceDetails: IInvoiceDetails[] = [];
+    invDetailCount: number;
+    data: ICustomReceipt;
+
+    constructor(private receiptnoteService: ReceiptnoteService, private activatedRoute: ActivatedRoute, private principal: Principal) {
+        this.principal.identity().then(account => {
+            this.currentUser = account;
+            this.receiptnote = new Receiptnote();
+            this.receiptnote.employeeId = this.currentUser.id;
+        });
+    }
 
     ngOnInit() {
         this.isSaving = false;
-        this.activatedRoute.data.subscribe(({ receiptnote }) => {
-            if (receiptnote !== undefined) {
-                this.receiptnote = receiptnote;
-            } else {
-                this.receiptnote = new Receiptnote();
-            }
-            this.createDate = this.receiptnote.createDate != null ? this.receiptnote.createDate.format(DATE_TIME_FORMAT) : null;
-            this.updateDate = this.receiptnote.updateDate != null ? this.receiptnote.updateDate.format(DATE_TIME_FORMAT) : null;
-        });
         this.activatedRoute.data.subscribe(({ invoiceHeader }) => {
             if (invoiceHeader !== undefined) {
                 this.invoiceHeader = invoiceHeader;
                 this.receiptnote.invoiceHeaderId = this.invoiceHeader.id;
+                this.receiptnoteService.getReceiptItemPackage({ id: this.invoiceHeader.id }).subscribe(res => {
+                    if (res.body != null) {
+                        this.lstInvoicePackage = res.body.invoicePackageList;
+                        this.lstInvoiceDetails = res.body.invoiceDetailList;
+                    }
+                });
             }
-        });
-        this.principal.identity().then(account => {
-            this.currentUser = account;
-            this.receiptnote.employeeId = this.currentUser.id;
         });
     }
 
@@ -53,14 +60,53 @@ export class ReceiptnoteUpdateComponent implements OnInit {
 
     save() {
         this.isSaving = true;
+        if (this.lstInvoicePackage.length === 0 && this.lstInvoiceDetails.length === 0) {
+            return;
+        }
         this.receiptnote.createDate =
             this.createDate != null ? moment(this.createDate, DATE_TIME_FORMAT) : moment(new Date(), DATE_TIME_FORMAT);
         this.receiptnote.updateDate = moment(new Date(), DATE_TIME_FORMAT);
         if (this.receiptnote.id !== undefined) {
             this.subscribeToSaveResponse(this.receiptnoteService.update(this.receiptnote));
         } else {
-            this.subscribeToSaveResponse(this.receiptnoteService.create(this.receiptnote));
+            this.data.receipt = this.receiptnote;
+            this.data.invoicePackageList = this.lstInvoicePackage;
+            this.data.invoiceDetailList = this.lstInvoiceDetails;
+            this.receiptnoteService.createReceiptNoteAndShipmentInvoice(this.data).subscribe(
+                (res: HttpResponse<any>) => {
+                    this.onSaveSuccess();
+                },
+                (res: HttpErrorResponse) => {
+                    this.onSaveError();
+                }
+            );
         }
+    }
+
+    addNewInvoiceDetailElement() {
+        this.invDetailCount++;
+        const obj = new InvoiceDetails(null, null, '', '', null, null, null, null, '', '', '', null, null);
+        this.lstInvoiceDetails.push(obj);
+        console.log(this.lstInvoiceDetails);
+    }
+
+    removeInvoiceDetailElement(index: any) {
+        this.invDetailCount--;
+        this.lstInvoiceDetails.splice(index, 1);
+        console.log(this.lstInvoiceDetails);
+    }
+
+    addNewInvoicePackageElement() {
+        this.invPackageCount++;
+        const obj = new InvoicePackage(null, null, null, null, null, null, null, false, 'New', '', null, null, null);
+        this.lstInvoicePackage.push(obj);
+        console.log(this.lstInvoicePackage);
+    }
+
+    removeInvoicePackageElement(index: any) {
+        this.invPackageCount--;
+        this.lstInvoicePackage.splice(index, 1);
+        console.log(this.lstInvoicePackage);
     }
 
     private subscribeToSaveResponse(result: Observable<HttpResponse<IReceiptnote>>) {
@@ -75,4 +121,10 @@ export class ReceiptnoteUpdateComponent implements OnInit {
     private onSaveError() {
         this.isSaving = false;
     }
+}
+
+export interface ICustomReceipt {
+    receipt?: IReceiptnote;
+    invoicePackageList?: IInvoicePackage[];
+    invoiceDetailList?: IInvoiceDetails[];
 }
