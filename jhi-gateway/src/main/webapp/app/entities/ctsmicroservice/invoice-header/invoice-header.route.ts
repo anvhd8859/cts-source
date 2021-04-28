2973a0874940e65ac70f57bed16d552c3747f025
anvhd8859 <43931722+anvhd8859@users.noreply.github.com>
2021-04-28 12:22:45 +0700

    Update invoice-header.route.ts

--- jhi-gateway/src/main/webapp/app/entities/ctsmicroservice/invoice-header/invoice-header.route.ts
+++ jhi-gateway/src/main/webapp/app/entities/ctsmicroservice/invoice-header/invoice-header.route.ts
@@ -48,7 +48,7 @@ export const invoiceHeaderRoute: Routes = [
             invoiceHeader: InvoiceHeaderResolve
         },
         data: {
-            authorities: ['ROLE_ADMIN', 'ROLE_SHIPPER', 'ROLE_OFFICER', 'ROLE_KEEPER', 'ROLE_USER'],
+            authorities: ['ROLE_ADMIN', 'ROLE_SHIPPER', 'ROLE_OFFICER', 'ROLE_KEEPER'],
             pageTitle: 'Invoice Service'
         },
         canActivate: [UserRouteAccessService]
