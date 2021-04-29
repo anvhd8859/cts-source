7831fe6f96c475d9b48e8af8f0e50c83dec420e3
tuyenvnt <77369982+tuyenvnt@users.noreply.github.com>
2021-04-30 01:12:34 +0700

    Create index.ts

---
+++ jhi-gateway/src/main/webapp/app/entities/ctsmicroservice/request-import-warehouse/index.ts
@@ -0,0 +1,6 @@
+export * from './request-import-warehouse.service';
+export * from './request-import-warehouse-update.component';
+export * from './request-import-warehouse-delete-dialog.component';
+export * from './request-import-warehouse-detail.component';
+export * from './request-import-warehouse.component';
+export * from './request-import-warehouse.route';
