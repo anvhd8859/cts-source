2b2bd73dcb15e3470cfd243f7b55e37bfd84f8ef
tuyenvnt <77369982+tuyenvnt@users.noreply.github.com>
2021-04-30 01:16:17 +0700

    Create RequestDetailsService.java

---
+++ jhi-srv/src/main/java/com/fu/capstone/service/RequestDetailsService.java
@@ -0,0 +1,43 @@
+package com.fu.capstone.service;
+
+import com.fu.capstone.service.dto.RequestDetailsDTO;
+
+import java.util.List;
+import java.util.Optional;
+
+/**
+ * Service Interface for managing RequestDetails.
+ */
+public interface RequestDetailsService {
+
+    /**
+     * Save a requestDetails.
+     *
+     * @param requestDetailsDTO the entity to save
+     * @return the persisted entity
+     */
+    RequestDetailsDTO save(RequestDetailsDTO requestDetailsDTO);
+
+    /**
+     * Get all the requestDetails.
+     *
+     * @return the list of entities
+     */
+    List<RequestDetailsDTO> findAll();
+
+
+    /**
+     * Get the "id" requestDetails.
+     *
+     * @param id the id of the entity
+     * @return the entity
+     */
+    Optional<RequestDetailsDTO> findOne(Long id);
+
+    /**
+     * Delete the "id" requestDetails.
+     *
+     * @param id the id of the entity
+     */
+    void delete(Long id);
+}
