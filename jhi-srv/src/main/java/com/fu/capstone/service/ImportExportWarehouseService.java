5d2a9b8b63c27f7c5e085c4b577af10297a9ecd1
tuyenvnt <77369982+tuyenvnt@users.noreply.github.com>
2021-04-30 01:15:42 +0700

    Create ImportExportWarehouseService.java

---
+++ jhi-srv/src/main/java/com/fu/capstone/service/ImportExportWarehouseService.java
@@ -0,0 +1,46 @@
+package com.fu.capstone.service;
+
+import com.fu.capstone.service.dto.ImportExportWarehouseDTO;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+import java.util.Optional;
+
+/**
+ * Service Interface for managing ImportExportWarehouse.
+ */
+public interface ImportExportWarehouseService {
+
+    /**
+     * Save a importExportWarehouse.
+     *
+     * @param importExportWarehouseDTO the entity to save
+     * @return the persisted entity
+     */
+    ImportExportWarehouseDTO save(ImportExportWarehouseDTO importExportWarehouseDTO);
+
+    /**
+     * Get all the importExportWarehouses.
+     *
+     * @param pageable the pagination information
+     * @return the list of entities
+     */
+    Page<ImportExportWarehouseDTO> findAll(Pageable pageable);
+
+
+    /**
+     * Get the "id" importExportWarehouse.
+     *
+     * @param id the id of the entity
+     * @return the entity
+     */
+    Optional<ImportExportWarehouseDTO> findOne(Long id);
+
+    /**
+     * Delete the "id" importExportWarehouse.
+     *
+     * @param id the id of the entity
+     */
+    void delete(Long id);
+}
