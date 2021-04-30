42140960e1eea76e2858e271fa977f579d08625f
tuyenvnt <77369982+tuyenvnt@users.noreply.github.com>
2021-04-30 23:29:06 +0700

    Update invoice-header.model.ts

--- jhi-gateway/src/main/webapp/app/shared/model/ctsmicroservice/invoice-header.model.ts
+++ jhi-gateway/src/main/webapp/app/shared/model/ctsmicroservice/invoice-header.model.ts
@@ -24,6 +24,7 @@ export interface IInvoiceHeader {
     cancelReason?: string;
     customerConfirm?: boolean;
     changeNote?: string;
+    reviewDate?: string;
     finish?: boolean;
     dueDate?: Moment;
     finishDate?: Moment;
@@ -57,6 +58,7 @@ export class InvoiceHeader implements IInvoiceHeader {
         public customerConfirm?: boolean,
         public changeNote?: string,
         public finish?: boolean,
+        public reviewDate?: string,
         public dueDate?: Moment,
         public finishDate?: Moment,
         public createDate?: Moment,
