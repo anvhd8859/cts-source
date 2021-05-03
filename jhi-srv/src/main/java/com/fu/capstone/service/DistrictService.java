b68d11a5f7a8169b482beb44343fd3f2acfe29ce
tuyenvnt <48380839+thangndse05808@users.noreply.github.com>
2021-05-03 12:44:33 +0700

    Update DistrictService.java

--- jhi-srv/src/main/java/com/fu/capstone/service/DistrictService.java
+++ jhi-srv/src/main/java/com/fu/capstone/service/DistrictService.java
@@ -1,3 +1,54 @@
+package com.fu.capstone.service;
+
+import com.fu.capstone.service.dto.DistrictDTO;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+import java.util.List;
+import java.util.Optional;
+
+/**
+ * Service Interface for managing District.
+ */
+public interface DistrictService {
+
+    /**
+     * Save a district.
+     *
+     * @param districtDTO the entity to save
+     * @return the persisted entity
+     */
+    DistrictDTO save(DistrictDTO districtDTO);
+
+    /**
+     * Get all the districts.
+     *
+     * @param pageable the pagination information
+     * @return the list of entities
+     */
+    Page<DistrictDTO> findAll(Pageable pageable);
+
+
+    /**
+     * Get the "id" district.
+     *
+     * @param id the id of the entity
+     * @return the entity
+     */
+    Optional<DistrictDTO> findOne(Long id);
+
+    /**
+     * Delete the "id" district.
+     *
+     * @param id the id of the entity
+     */
+    void delete(Long id);
+
+	List<DistrictDTO> getDistrictByProvinceId(Long id);
+
+	DistrictDTO getDistrictByStreetId(Long id);
+}
 
 
 
