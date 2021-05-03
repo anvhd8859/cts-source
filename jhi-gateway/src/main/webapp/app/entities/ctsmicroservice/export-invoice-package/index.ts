8e36d5b4d06b75fdfe3a258edcf1a9578144da49
tuyenvnt <77369982+tuyenvnt@users.noreply.github.com>
2021-05-03 19:55:22 +0700

    Update index.ts

--- jhi-gateway/src/main/webapp/app/entities/ctsmicroservice/export-invoice-package/index.ts
+++ jhi-gateway/src/main/webapp/app/entities/ctsmicroservice/export-invoice-package/index.ts
@@ -4,3 +4,4 @@ export * from './export-invoice-package-delete-dialog.component';
 export * from './export-invoice-package-detail.component';
 export * from './export-invoice-package.component';
 export * from './export-invoice-package.route';
+export * from './export-invoice-modal-warning.component';
