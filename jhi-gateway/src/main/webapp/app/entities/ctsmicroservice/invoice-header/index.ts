22399c8d92d9220bb432afdf504d997af4650ba2
Hainm149889 <hainm149889@gmail.com>
2021-05-03 22:07:59 +0700

    Update index.ts

--- jhi-gateway/src/main/webapp/app/entities/ctsmicroservice/invoice-header/index.ts
+++ jhi-gateway/src/main/webapp/app/entities/ctsmicroservice/invoice-header/index.ts
@@ -5,6 +5,7 @@ export * from './invoice-header-user.component';
 export * from './invoice-header-review.component';
 export * from './invoice-header-delete-dialog.component';
 export * from './invoice-header-finish-dialog.component';
+export * from './invoice-header-pricing-dialog.component';
 export * from './invoice-header-detail.component';
 export * from './invoice-header.component';
 export * from './invoice-header.route';
