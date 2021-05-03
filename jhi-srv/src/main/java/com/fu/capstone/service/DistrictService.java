8f3f2bbbc547ca549b3a280de3688f60723677fe
tuyenvnt <48380839+thangndse05808@users.noreply.github.com>
2021-05-03 12:43:41 +0700

    Update DistrictService.java

--- jhi-srv/src/main/java/com/fu/capstone/service/DistrictService.java
+++ jhi-srv/src/main/java/com/fu/capstone/service/DistrictService.java
@@ -3,7 +3,6 @@ package com.fu.capstone.service;
 import com.fu.capstone.service.dto.DistrictDTO;
 
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 
 
 
