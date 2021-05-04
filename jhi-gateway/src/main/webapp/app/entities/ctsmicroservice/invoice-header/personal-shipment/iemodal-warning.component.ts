912aab319f19874650ab8b2e2bb45cfa1342e999
Hainm149889 <hainm149889@gmail.com>
2021-05-05 01:44:49 +0700

    Create iemodal-warning.component.ts

---
+++ jhi-gateway/src/main/webapp/app/entities/ctsmicroservice/invoice-header/personal-shipment/iemodal-warning.component.ts
@@ -0,0 +1,41 @@
+import { Component, Directive } from '@angular/core';
+import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
+
+@Component({
+    selector: 'jhi-iemodal-warning-component',
+    template: `
+    <div class="modal-header">
+      <h4 class="modal-title" id="modal-title">Warning</h4>
+      <button
+        type="button"
+        class="close"
+        aria-describedby="modal-title"
+        (click)="modal.dismiss('Cross click')"
+      >
+        <span aria-hidden="true">&times;</span>
+      </button>
+    </div>
+    <div class="modal-body">
+      <p>
+        <strong>Bạn lựa chọn một số hóa đơn đã yêu cầu xuất nhập kho hoặc không đủ điều kiện</strong>
+      </p>
+    </div>
+    <div class="modal-footer">
+      <div
+        style="width: 20%; width: 30px;"
+      >
+      </div>
+      <button
+        style="margin-left: 51%; width: 20%; margin-right:5%"
+        type="button"
+        class="btn btn-warning"
+        (click)="modal.close('Ok click')"
+      >
+        Ok
+      </button>
+    </div>
+  `
+})
+export class IEModalWarningComponent {
+    constructor(public modal: NgbActiveModal) {}
+}
