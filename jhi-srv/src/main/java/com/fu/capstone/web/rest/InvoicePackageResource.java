c7f361d1cdff41009d40e4d0fde59df6177fc4e9
tuyenvnt <77369982+tuyenvnt@users.noreply.github.com>
2021-05-04 14:45:58 +0700

    fix

--- jhi-srv/src/main/java/com/fu/capstone/web/rest/InvoicePackageResource.java
+++ jhi-srv/src/main/java/com/fu/capstone/web/rest/InvoicePackageResource.java
@@ -172,9 +172,9 @@ public class InvoicePackageResource {
     @GetMapping("/invoice-packages/export-package")
     @Timed
     public ResponseEntity<List<InvoicePackageShipmentDTO>> getExportPackageByOfficeId(@RequestParam("id") Long id
-    		,@RequestParam("invNo") String invNo, @RequestParam("type") String type, @RequestParam("fromDate") String fromDate,
+    		,@RequestParam("invNo") String invNo, @RequestParam("status") String status, @RequestParam("fromDate") String fromDate,
 			@RequestParam("toDate") String toDate, Pageable pageable) {
-    	Page<InvoicePackageShipmentDTO> page = invoicePackageService.getExportPackageByOfficeId(id, invNo, type, fromDate, toDate,  pageable);
+    	Page<InvoicePackageShipmentDTO> page = invoicePackageService.getExportPackageByOfficeId(id, invNo, status, fromDate, toDate,  pageable);
     	HttpHeaders headers = PaginationUtil.generatePaginationHttpHeaders(page, "/api/invoice-packages/export-package");
         return new ResponseEntity<>(page.getContent(), headers, HttpStatus.OK);
     }
