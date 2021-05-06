8647bff4425340b45f029275a44edba55a825abb
tuyenvnt <77369982+tuyenvnt@users.noreply.github.com>
2021-05-07 01:46:09 +0700

    UPDATE

--- jhi-gateway/src/main/webapp/app/entities/ctsmicroservice/receipt-image/index.ts
+++
@@ -1,6 +0,0 @@
-export * from './receipt-image.service';
-export * from './receipt-image-update.component';
-export * from './receipt-image-delete-dialog.component';
-export * from './receipt-image-detail.component';
-export * from './receipt-image.component';
-export * from './receipt-image.route';
