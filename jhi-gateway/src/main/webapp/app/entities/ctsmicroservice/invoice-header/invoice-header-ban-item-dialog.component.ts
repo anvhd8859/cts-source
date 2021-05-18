1362a880fa46206991323dc658abafd3468d8544
Hainm149889 <hainm149889@gmail.com>
2021-05-18 19:04:44 +0700

    Create invoice-header-ban-item-dialog.component.ts

---
+++ jhi-gateway/src/main/webapp/app/entities/ctsmicroservice/invoice-header/invoice-header-ban-item-dialog.component.ts
@@ -0,0 +1,62 @@
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+
+import { NgbActiveModal, NgbModal, NgbModalRef } from '@ng-bootstrap/ng-bootstrap';
+import { JhiEventManager } from 'ng-jhipster';
+
+import { IInvoiceHeader } from 'app/shared/model/ctsmicroservice/invoice-header.model';
+import { InvoiceHeaderService } from './invoice-header.service';
+
+@Component({
+    selector: 'jhi-invoice-header-ban-item-dialog',
+    templateUrl: './invoice-header-ban-item-dialog.component.html'
+})
+export class InvoiceHeaderBanItemDialogComponent {
+    invoiceHeader: IInvoiceHeader;
+
+    constructor(
+        private invoiceHeaderService: InvoiceHeaderService,
+        public activeModal: NgbActiveModal,
+        private eventManager: JhiEventManager
+    ) {}
+
+    clear() {
+        this.activeModal.dismiss('cancel');
+    }
+}
+
+@Component({
+    selector: 'jhi-invoice-header-delete-popup',
+    template: ''
+})
+export class InvoiceHeaderBanItemPopupComponent implements OnInit, OnDestroy {
+    private ngbModalRef: NgbModalRef;
+
+    constructor(private activatedRoute: ActivatedRoute, private router: Router, private modalService: NgbModal) {}
+
+    ngOnInit() {
+        this.activatedRoute.data.subscribe(({ invoiceHeader }) => {
+            setTimeout(() => {
+                this.ngbModalRef = this.modalService.open(InvoiceHeaderBanItemDialogComponent as Component, {
+                    size: 'lg',
+                    backdrop: 'static'
+                });
+                this.ngbModalRef.componentInstance.invoiceHeader = invoiceHeader;
+                this.ngbModalRef.result.then(
+                    result => {
+                        this.router.navigate([{ outlets: { popup: null } }], { replaceUrl: true, queryParamsHandling: 'merge' });
+                        this.ngbModalRef = null;
+                    },
+                    reason => {
+                        this.router.navigate([{ outlets: { popup: null } }], { replaceUrl: true, queryParamsHandling: 'merge' });
+                        this.ngbModalRef = null;
+                    }
+                );
+            }, 0);
+        });
+    }
+
+    ngOnDestroy() {
+        this.ngbModalRef = null;
+    }
+}
