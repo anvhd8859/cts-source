764ca5c956d93e152567d751e5c760e5a9e50db5
Hainm149889 <hainm149889@gmail.com>
2021-07-25 22:41:54 +0700

    update warehouse UI

--- jhi-gateway/src/main/webapp/app/entities/ctsmicroservice/warehouse/warehouse-detail.component.ts
+++ jhi-gateway/src/main/webapp/app/entities/ctsmicroservice/warehouse/warehouse-detail.component.ts
@@ -3,6 +3,8 @@ import { ActivatedRoute } from '@angular/router';
 
 import { IWarehouse } from 'app/shared/model/ctsmicroservice/warehouse.model';
 import { IOffice } from './../../../shared/model/ctsmicroservice/office.model';
+import { OfficeService } from './../office/office.service';
+import { HttpResponse } from '@angular/common/http';
 
 @Component({
     selector: 'jhi-warehouse-detail',
@@ -12,12 +14,14 @@ export class WarehouseDetailComponent implements OnInit {
     warehouse: IWarehouse;
     office: IOffice;
 
-    constructor(private activatedRoute: ActivatedRoute) {}
+    constructor(private activatedRoute: ActivatedRoute, private OfficeService: OfficeService) {}
 
     ngOnInit() {
-        this.activatedRoute.data.subscribe(({ warehouse, office }) => {
+        this.activatedRoute.data.subscribe(warehouse => {
             this.warehouse = warehouse;
-            this.office = office;
+            this.OfficeService.find(this.warehouse.officeId).subscribe((res: HttpResponse<IOffice>) => {
+                this.office = res.body;
+            });
         });
     }
 
