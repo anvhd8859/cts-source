b89e9aac419fd45401dd638f8a224951ec871873
dongph1-13012021 <donghoangpham1995@gmail.com>
2021-08-25 11:35:02 +0700

    t#

--- jhi-srv/src/main/java/com/fu/capstone/web/rest/WarehouseResource.java
+++ jhi-srv/src/main/java/com/fu/capstone/web/rest/WarehouseResource.java
@@ -1,26 +1,31 @@
 package com.fu.capstone.web.rest;
 
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.List;
+import java.util.Optional;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 import com.codahale.metrics.annotation.Timed;
 import com.fu.capstone.service.WarehouseService;
+import com.fu.capstone.service.dto.WarehouseDTO;
 import com.fu.capstone.service.dto.WarehouseDetailDTO;
 import com.fu.capstone.web.rest.errors.BadRequestAlertException;
 import com.fu.capstone.web.rest.util.HeaderUtil;
-import com.fu.capstone.service.dto.WarehouseDTO;
-import io.github.jhipster.web.util.ResponseUtil;;
-import org.codehaus.jettison.json.JSONArray;
-import org.codehaus.jettison.json.JSONObject;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
 
-import java.net.URI;
-import java.net.URISyntaxException;
-
-import java.util.List;
-import java.util.Optional;
+import io.github.jhipster.web.util.ResponseUtil;
 
 /**
  * REST controller for managing Warehouse.
